34d1cfb692383d11727086f2dc87b69d331f4fab
zilaei <zzilaei@gmail.com>
2023-08-01 10:08:27 +0200

    feat(cli): init main class

--- src/f_ai/cli/__main__.py
+++ src/f_ai/cli/__main__.py
@@ -1,5 +1,6 @@
+
 import sys
-from f_ai.argument_handler.arg_parsing import ArgumentHandler
+from f_ai.argument_handler.arg_parsing import ArgumentHandler, ArgumentStatusCodes
 
 CLI_TITLE = "F-AI CLI"
 
@@ -8,11 +9,32 @@ class CLI:
     def __init__(self, title=CLI_TITLE):
         self.title = title
 
+    def start_command_factory(self, model_name: str, server_type: str):
+        # we can have various mode-server types which will have different ways to start them
+        # each type is mapped to a corresponding class that knows how to start it
+        command_class_map = {
+            # ('parrot_fml', 'http'): Model1ServerType1Command,
+        }
+
+        command_class = command_class_map.get((model_name, server_type))
+        if command_class is None:
+            raise ValueError(f"Unknown model-server type combination: {model_name}-{server_type}")
+
+        command_object = command_class()
+        return command_object
+
     def run(self, args: list):
         argument_handler = ArgumentHandler(args, self.title)
         status_code = argument_handler.handle_args(sys.argv)
         if status_code == ArgumentStatusCodes.EXIT_APPLICATION:
             sys.exit(0)
+
+        # TODO: assuming we have a model name, should be checked in argument_handler
+        model_name = argument_handler.args.model_name
+        server_type = argument_handler.args.server_type
+
+        command = self.start_command_factory(model_name, server_type)
+        command.execute()
 
         print("Run not implemented. Exiting...")
         sys.exit(0)
