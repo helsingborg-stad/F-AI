ee57e8254432aafa553b580cafbf74e113d025cb
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2023-09-12 19:18:39 +0200

    fix

--- src/planning_permission/app_main_stream.py
+++ src/planning_permission/app_main_stream.py
@@ -6,7 +6,6 @@ from dotenv import load_dotenv
 from langstream import debug, Stream
 import chainlit as cl
 
-from planning_permission.streams.collection import get_scoring_stream, get_query_openai
 from planning_permission.store.document_store import DocumentStore
 from planning_permission.utils.embeddings_handler import OpenAIGenerator
 from planning_permission.store.database import ChromaDB
