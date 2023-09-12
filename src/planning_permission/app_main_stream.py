3d035f4214e288d876a6b193f794b9524f0b545c
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2023-09-12 18:39:37 +0200

    refactor main loop

--- src/planning_permission/app_main_stream.py
+++ src/planning_permission/app_main_stream.py
@@ -1,7 +1,8 @@
 import os
-from typing import Dict, Tuple, List
-from dotenv import load_dotenv
+import json
 
+from typing import Dict, Optional
+from dotenv import load_dotenv
 from langstream import debug, Stream
 import chainlit as cl
 
@@ -9,6 +10,16 @@ from planning_permission.streams.collection import get_scoring_stream, get_query
 from planning_permission.store.document_store import DocumentStore
 from planning_permission.utils.embeddings_handler import OpenAIGenerator
 from planning_permission.store.database import ChromaDB
+
+from numpy import add
+from typing import Dict, Callable, Optional
+from langstream import debug, Stream
+from langstream.contrib import OpenAIChatDelta
+
+from planning_permission.chat.stream import create_chat_stream_from_prompt
+from planning_permission.chat.prompt import ChatPrompt
+from planning_permission.chat.template import CHAT_PROMPT_TEMPLATE_ARGS, SCORING_PROMPT_TEMPLATE_ARGS
+
 
 load_dotenv(dotenv_path="./.env")
 DB_DIRECTORY = os.environ.get("DB_PATH", "./f-ai.db")
@@ -24,52 +35,74 @@ document_store = DocumentStore(
 if document_store.is_collection_empty():
     document_store.load_document(pathname=DOCUMENTS_TO_EMBED)
 
+DEBUG_STREAM = os.environ.get("DEBUG_STREAM", False)
 
-def stream_with_scoring(query):
+def use_chat_stream(query: str, debug_fn: Optional[Callable] = None )-> tuple[Stream[str, OpenAIChatDelta], Dict[str, ChatPrompt]]:
     add_document, list_documents = (lambda documents: (
         lambda document: (documents.append(document), document)[1],
         lambda: [*documents]
     ))([])
+    
+    chat_stream, chat_prompt = create_chat_stream_from_prompt(CHAT_PROMPT_TEMPLATE_ARGS)
+    scoring_stream, scoring_prompt = create_chat_stream_from_prompt(SCORING_PROMPT_TEMPLATE_ARGS)
+    
+    scoring_stream = scoring_stream.map(
+        lambda delta: (lambda num: num)(json.loads(delta.content)['score'])
+        if delta.role == "function" and delta.name == "score_document"
+        else 0
+    )
 
-    scoring_stream = get_scoring_stream(query)
-    query_openai = get_query_openai(query)
+    stream = (
+        Stream[str, str](
+            "RetrieveDocumentsStream",
+            lambda query: document_store.query(query=query, n_results=10)
+        )
+        .map(add_document)
+        .map(lambda document: {"query": query, "document": document})
+        .map(scoring_stream)
+        .gather()
+        .and_then(lambda scores: zip(list_documents(), [s[0] for s in scores]))
+        .and_then(lambda scored: sorted(list(scored)[0], key=lambda x: x[1], reverse=True)[:4])
+        .and_then(lambda results: {"query": query, "results": results[0]})
+        .and_then(chat_stream)
+    )
 
     return (
-        debug(
-            Stream[str, str](
-                "RetrieveDocumentsStream",
-                lambda query: document_store.query(query=query, n_results=10)
-            )
-            .map(add_document)
-            .map(scoring_stream)
-            .gather()
-            .and_then(lambda scores: zip(list_documents(), [s[0] for s in scores]))
-            .and_then(lambda scored: sorted(list(scored)[0], key=lambda x: x[1], reverse=True)[:4])
-            .and_then(query_openai)
-        )
-    )(query)
-
+        (debug_fn or (lambda x: x))(stream), 
+        {
+            chat_prompt.name: chat_prompt,
+            scoring_prompt.name: scoring_prompt,
+        }
+    )
 
 @cl.on_message
 async def on_message(message: str):
-    messages_map: Dict[str, Tuple[bool, cl.Message]] = {}
-
-    async for output in stream_with_scoring(message):
+    stream, prompts = use_chat_stream(message, debug if DEBUG_STREAM else None)
+   
+    cl_messages_map: Dict[str, cl.Message] = {}
+    
+    async for output in stream(message):
         if "@" in output.stream and not output.final:
             continue
-        if output.stream in messages_map:
-            sent, cl_message = messages_map[output.stream]
-            if not sent:
-                await cl_message.send()
-                messages_map[output.stream] = (False, cl_message)
-            await cl_message.stream_token(output.data.content)
+
+        if output.stream in cl_messages_map:
+            cl_message = cl_messages_map[output.stream]
+            await cl_message.stream_token(
+                output.data.content if hasattr(output.data, "content") else ""
+            )
         else:
             if hasattr(output.data, "content"):
-                messages_map[output.stream] = (
-                    True,
-                    cl.Message(
-                        author=output.stream,
-                        content=output.data.content,
-                        indent=0 if output.final else 1,
-                    ),
+                cl_messages_map[output.stream] = cl.Message(
+                    author=output.stream,
+                    content= output.data.content,
+                    indent=0 if output.final else 1,
+                    prompt=prompts[output.stream].to_prompt() if output.stream in prompts else None
                 )
+
+    for cl_message in cl_messages_map.values():
+        if cl_message.prompt:
+            cl_message.prompt.completion = cl_message.content or ""
+        await cl_message.send()
+ 
+            
+
