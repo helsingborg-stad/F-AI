3b377a7439daf4c342171cb708bcf5d050dcdad5
zilaei <zzilaei@gmail.com>
2023-10-10 16:38:57 +0200

    add tests

--- src/planning_permission/tests/test_file_handler.py
+++ src/planning_permission/tests/test_file_handler.py
@@ -1,6 +1,6 @@
 import unittest
-from unittest.mock import MagicMock
-from planning_permission.utils.file_handler import DocumentHandler, AbstractDocumentParser
+from unittest.mock import MagicMock, mock_open, patch
+from planning_permission.utils.file_handler import DocumentHandler, AbstractDocumentParser, FileUpload, FileObject
 
 
 class TestChunkStringsMethod(unittest.TestCase):
@@ -85,6 +85,43 @@ class TestParseDocsMethod(unittest.TestCase):
         )
         mock_print.assert_any_call("Parsing file1.md...")
         mock_print.assert_any_call("Parsing file2.pdf...")
+
+
+class TestFileUpload(unittest.TestCase):
+
+    def setUp(self):
+        self.storage_path = "/path/to/storage"
+        self.file_upload = FileUpload(self.storage_path)
+
+    def test_save_file(self):
+        file_objects = [
+            FileObject(name="file1.txt", path="", size=15, type="text/plain", content=b"Hello, world!"),
+            FileObject(name="file2.txt", path="", size=12, type="text/plain", content=b"Hello, File!"),
+        ]
+        destination_directory = "/path/to/destination"
+
+        m = mock_open()
+        with patch('builtins.open', m), patch('os.makedirs') as mock_makedirs:
+            self.file_upload.save_file(file_objects, destination_directory)
+
+        mock_makedirs.assert_called_once_with(destination_directory, exist_ok=True)
+        m.assert_any_call('/path/to/destination/file1.txt', 'wb')
+        m.assert_any_call('/path/to/destination/file2.txt', 'wb')
+        m().write.assert_any_call(b"Hello, world!")
+        m().write.assert_any_call(b"Hello, File!")
+
+    def test_handle_uploaded_files(self):
+        file_objects = [
+            FileObject(name="file3.txt", path="", size=15, type="text/plain", content=b"Hello, world!"),
+            FileObject(name="file4.txt", path="", size=12, type="text/plain", content=b"Hello, File!"),
+        ]
+
+        with patch.object(self.file_upload, 'save_file') as mock_save_file:
+            import asyncio
+            loop = asyncio.get_event_loop()
+            loop.run_until_complete(self.file_upload.handle_uploaded_files(file_objects))
+
+        mock_save_file.assert_called_once_with(file_objects=file_objects, destination_directory=self.storage_path)
 
 
 if __name__ == '__main__':
