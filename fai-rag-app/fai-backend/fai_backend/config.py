844bdc0f1137b9aecef78ab9dfbf82bcc1d0cbef
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2024-01-16 11:39:58 +0100

    set default envs

--- fai-rag-app/fai-backend/fai_backend/config.py
+++ fai-rag-app/fai-backend/fai_backend/config.py
@@ -2,27 +2,21 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    APP_DB: str = 'memory'
+    APP_PROJECT_NAME: str = 'fai-rag-app'
+    APP_ADMIN_EMAIL: str
+    APP_DB: str = 'mongodb'
     SECRET_KEY: str
     ALGORITHM: str = 'HS256'
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-    ENV_MODE: str = 'development'
-
-    MONGO_DB_NAME: str
+    ENV_MODE: str = 'production'
+    MONGO_DB_NAME: str = 'fai-rag-app'
     MONGO_DB_URI: str = 'mongodb://localhost:27017'
     FIXED_PIN: int | None = None
-
-    APP_PROJECT_NAME: str
-    APP_ADMIN_EMAIL: str
-
     MAIL_CLIENT: str
     MAIL_SENDER_EMAIL: str
     MAIL_SENDER_NAME: str
-
     BREVO_API_URL: str
     BREVO_API_KEY: str
-
-    LOG_LEVEL: str
+    LOG_LEVEL: str = 'INFO'
 
     class Config:
         env_file = '.env'
