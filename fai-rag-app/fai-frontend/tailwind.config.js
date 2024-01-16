e23e3a0c3671fbde43cfb0ac12569f0981758fa6
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2024-01-16 12:06:32 +0100

    look for tailwind css class def in backend also

--- fai-rag-app/fai-frontend/tailwind.config.js
+++ fai-rag-app/fai-frontend/tailwind.config.js
@@ -2,7 +2,8 @@
 export default {
     content: [
         "./index.html",
-        "./src/**/**/*.{svelte,js,ts,jsx,tsx}"
+        "./src/**/**/*.{svelte,js,ts,jsx,tsx}",
+        "../fai-backend/fai_backend/**/**/*.py",
     ],
     theme: {
         extend: {},
