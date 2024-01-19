aed1efa26bb2a861175c4670b5e42876b8d03de7
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2024-01-19 16:33:10 +0100

    refactor: page_template view

--- fai-rag-app/fai-backend/fai_backend/views.py
+++ fai-rag-app/fai-backend/fai_backend/views.py
@@ -1,37 +1,82 @@
-from fastui import AnyComponent
-from fastui import components as c
-from fastui.events import GoToEvent
+from fai_backend.framework import components as c
+from fai_backend.phrase import phrase as _
 
 
-def page_template(
-        *components: AnyComponent,
-        title: str | None = None,
-) -> list[AnyComponent]:
+def page_template(*components, page_title: str = None) -> list:
+    def page_title_component() -> list:
+        return [c.Heading(
+            text=page_title,
+            class_name='flex-none',
+        )] if page_title else []
+
     return [
-        c.PageTitle(text=f'FastUI Demo — {title}' if title else 'FastUI Demo'),
-        c.Navbar(
-            title='Folkets Ai',
-            title_event=GoToEvent(url='/'),
-            links=[
-                c.Link(
-                    components=[c.Text(text='Conversations')],
-                    on_click=GoToEvent(url='/conversations'),
-                    active='startswith:/components',
-                ),
-                c.Link(
-                    components=[c.Text(text='Logga ut')],
-                    on_click=GoToEvent(url='/logout'),
-                ),
-            ],
-        ),
-        c.Page(
+        c.AppShell(
+            hasDrawer=True,
             components=[
-                *((c.Heading(text=title),) if title else ()),
-                *components,
+                c.AppDrawer(
+                    slot='drawer',
+                    title='Folkets AI',
+                    components=[
+                        c.Menu(
+                            title=_('questions', 'Questions'),
+                            id='questions-menu',
+                            variant='vertical',
+                            components=[
+                                c.Link(
+                                    title=_('submit_question', 'Submit a question'),
+                                    url='/',
+                                ),
+                                c.Menu(
+                                    title=_('my_questions', 'My Questions'),
+                                    id='questions-menu',
+                                    variant='vertical',
+                                    sub_menu=True,
+                                    components=[
+                                        c.Link(
+                                            title=_('show_all', 'Visa alla'),
+                                            url='/questions'
+                                        ),
+                                    ],
+                                ),
+                            ],
+                        ),
+                    ]
+                ),
+                c.AppContent(
+                    components=[
+                        c.PageHeader(
+                            fixed=True,
+                            components=[
+                                *page_title_component(),
+                                c.Menu(
+                                    id='toolbar-menu',
+                                    variant='horizontal',
+                                    class_name='flex flex-row flex-1 justify-end',
+                                    components=[
+                                        c.Link(
+                                            title=_('logout_button_text', 'Logout'),
+                                            url='/logout',
+                                            icon='data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLWxvZy1vdXQiPjxwYXRoIGQ9Ik05IDIxSDVhMiAyIDAgMCAxLTItMlY1YTIgMiAwIDAgMSAyLTJoNCIvPjxwb2x5bGluZSBwb2ludHM9IjE2IDE3IDIxIDEyIDE2IDciLz48bGluZSB4MT0iMjEiIHgyPSI5IiB5MT0iMTIiIHkyPSIxMiIvPjwvc3ZnPg=='
+                                        ),
+                                    ],
+                                ),
+                            ]
+                        ),
+                        c.PageContent(
+                            class_name='mt-24',
+                            components=[
+                                *components,
+                            ]
+                        ),
+                        c.AppFooter(
+                            components=[
+                                c.Heading(
+                                    text='folketsAi',
+                                ),
+                            ]
+                        )
+                    ],
+                ),
             ],
-        ),
-        c.Footer(
-            extra_text='Folkets AI',
-            links=[],
-        ),
+        )
     ]
