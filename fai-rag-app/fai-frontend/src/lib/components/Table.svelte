bb7947698500b03ca2aab869221cfc0d242f0ba6
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2024-02-26 14:53:40 +0100

    fix(Svelte): force rerender of Table component when columns or data changes

--- fai-rag-app/fai-frontend/src/lib/components/Table.svelte
+++ fai-rag-app/fai-frontend/src/lib/components/Table.svelte
@@ -18,34 +18,38 @@
 {#if columns.length > 0}
     <table class:table={true} class:border={false} class={className}>
         <thead>
-        <tr class={headerClass}>
-            {#each columns as column}
-                <th class={cellClass}>
-                    <slot name="header" column={column}>{column.label}</slot>
-                </th>
-            {/each}
-        </tr>
+        {#key columns}
+            <tr class={headerClass}>
+                {#each columns as column}
+                    <th class={cellClass}>
+                        <slot name="header" column={column}>{column.label}</slot>
+                    </th>
+                {/each}
+            </tr>
+        {/key}
         </thead>
 
         <tbody>
-        {#each data as rowData, rowIndex}
-            <tr class={rowClass}>
-                {#each columns as column, colIndex}
-                    <td class={cellClass}>
-                        <slot name="cell" rowData={rowData} column={column} rowIndex={rowIndex} colIndex={colIndex}>
-                            {#if column.link_text}
-                                <Link underline="always"
-                                      href={rowData[column.key]}>{column.link_text}</Link>
-                            {/if}
+        {#key data}
+            {#each data as rowData, rowIndex}
+                <tr class={rowClass}>
+                    {#each columns as column, colIndex}
+                        <td class={cellClass}>
+                            <slot name="cell" rowData={rowData} column={column} rowIndex={rowIndex} colIndex={colIndex}>
+                                {#if column.link_text}
+                                    <Link underline="always"
+                                          href={rowData[column.key]}>{column.link_text}</Link>
+                                {/if}
 
-                            {#if !column.link_text}
-                                {rowData[column.key]}
-                            {/if}
-                        </slot>
-                    </td>
-                {/each}
-            </tr>
-        {/each}
+                                {#if !column.link_text}
+                                    {rowData[column.key]}
+                                {/if}
+                            </slot>
+                        </td>
+                    {/each}
+                </tr>
+            {/each}
+        {/key}
         </tbody>
     </table>
 {/if}
