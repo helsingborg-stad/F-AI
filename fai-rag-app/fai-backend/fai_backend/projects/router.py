28264db8e54b86dcf3333604cd86ac0701c9b437
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2024-03-14 09:50:07 +0100

    add: project user table view

--- fai-rag-app/fai-backend/fai_backend/projects/router.py
+++ fai-rag-app/fai-backend/fai_backend/projects/router.py
@@ -1,20 +1,25 @@
 from fastapi import APIRouter, Depends, Security
 
-from fai_backend.dependencies import get_authenticated_user
+from fai_backend.dependencies import get_authenticated_user, get_page_template_for_logged_in_users, get_project_user
+from fai_backend.framework import components as c
 from fai_backend.logger.route_class import APIRouter as LoggingAPIRouter
+from fai_backend.phrase import phrase as _
 from fai_backend.projects.dependencies import (
     create_project_request,
     delete_project_request,
     get_project_request,
+    get_project_service,
     list_projects_request,
     update_project_request,
 )
 from fai_backend.projects.schema import (
     ProjectResponse,
 )
+from fai_backend.projects.service import ProjectService
+from fai_backend.schema import ProjectUser
 
 router = APIRouter(
-    prefix='/v1',
+    prefix='/api',
     tags=['Projects'],
     route_class=LoggingAPIRouter,
     dependencies=[],
@@ -41,6 +46,69 @@ async def read_projects(
         projects: list[ProjectResponse] = Depends(list_projects_request),
 ):
     return projects
+
+
+@router.get('/users', response_model=list, response_model_exclude_none=True)
+async def list_project_users(
+        view=Depends(get_page_template_for_logged_in_users),
+        project_service: ProjectService = Depends(get_project_service),
+        project_user: ProjectUser = Depends(get_project_user),
+) -> list:
+    project = await project_service.read_project(project_user.project_id)
+
+    return view(
+        [c.Div(components=[
+            c.Div(components=[
+                c.Table(
+                    data=[
+                        {
+                            'email': user.email,
+                            'role': user.role,
+                        }
+                        for user in project.members
+                    ],
+                    columns=[
+                        {'key': 'email', 'label': _('email', 'Email')},
+                        {'key': 'role', 'label': _('role', 'Role')},
+                    ],
+
+                    class_name='text-base-content join-item md:table-sm lg:table-md table-auto',
+                ),
+            ], class_name='overflow-x-auto space-y-4'),
+        ], class_name='card bg-base-100 w-full max-w-6xl')],
+        _('users', 'Users'),
+    )
+
+
+@router.get('/roles', response_model=list, response_model_exclude_none=True)
+async def list_project_users(
+        view=Depends(get_page_template_for_logged_in_users),
+        project_service: ProjectService = Depends(get_project_service),
+        project_user: ProjectUser = Depends(get_project_user),
+) -> list:
+    project = await project_service.read_project(project_user.project_id)
+
+    return view(
+        [c.Div(components=[
+            c.Div(components=[
+                c.Table(
+                    data=[
+                        {
+
+                        }
+                        for role in project.roles
+                    ],
+                    columns=[
+                        {'key': role, 'label': role}
+                        for role in project.roles.keys()
+                    ],
+
+                    class_name='text-base-content join-item md:table-sm lg:table-md table-auto',
+                ),
+            ], class_name='overflow-x-auto space-y-4'),
+        ], class_name='card bg-base-100 w-full max-w-6xl')],
+        _('users', 'Users'),
+    )
 
 
 @router.get(
