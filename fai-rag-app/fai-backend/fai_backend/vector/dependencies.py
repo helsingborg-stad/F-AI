6a94e68173d0a3c62ad4672609fc7b982424f15c
zilaei <zzilaei@gmail.com>
2024-05-02 14:13:41 +0200

    fix vector service
    - Remove init of collection.
    - Fix async methods.

--- fai-rag-app/fai-backend/fai_backend/vector/dependencies.py
+++ fai-rag-app/fai-backend/fai_backend/vector/dependencies.py
@@ -2,7 +2,5 @@ from fai_backend.vector.service import VectorService
 from fai_backend.vector.factory import vector_db
 
 
-async def get_vector_service(
-        collection_name: str,
-) -> VectorService:
-    return VectorService(vector_db=vector_db, collection_name=collection_name)
+async def get_vector_service():
+    return VectorService(vector_db)
--- fai-rag-app/fai-backend/fai_backend/vector/service.py
+++ fai-rag-app/fai-backend/fai_backend/vector/service.py
@@ -1,67 +1,44 @@
-from typing import Optional, Dict
+from typing import Optional
 
 from fai_backend.vector.interface import IVector
-from fai_backend.vector.types import OneOrMany, Embedding
+from fai_backend.vector.types import OneOrMany
 
 
 class VectorService:
     vector_db: IVector
-    collection_name: str
 
-    def __init__(self, vector_db: IVector, collection_name: str):
+    def __init__(self, vector_db):
         self.vector_db = vector_db
-        self.collection_name = collection_name
 
-        self.vector_db.create_collection(collection_name=self.collection_name)
+    async def create_collection(self, collection_name: str):
+        await self.vector_db.create_collection(collection_name)
 
-    @staticmethod
-    def _generate_metadata(user_email: str, project_id: str, metadata_version: str = "1.0.0") -> Dict:
-        delimiter = "|"
+    async def delete_collection(self, collection_name: str):
+        await self.vector_db.delete_collection(collection_name)
 
-        metadata_tag = (
-            f"version:{metadata_version}{delimiter}"
-            f"project_id:{project_id}{delimiter}"
-            f"user_email:{user_email}"
-        )
-        return {
-            "project_user_id": metadata_tag
-        }
-
-    def add_vector_to_project(
+    async def add_to_collection(
             self,
-            user_email: str,
-            project_id: str,
+            collection_name: str,
             ids: OneOrMany[str],
-            embeddings: Optional[OneOrMany[Embedding]] = None,
-            documents: Optional[OneOrMany[str]] = None,
-            uris: Optional[OneOrMany[str]] = None,
+            documents: Optional[OneOrMany[str]],
     ):
-        metadata = self._generate_metadata(user_email, project_id)
-
-        self.vector_db.add(
-            collection_name=self.collection_name,
+        await self.vector_db.add(
+            collection_name=collection_name,
             ids=ids,
-            embeddings=embeddings,
             documents=documents,
-            uris=uris,
-            metadatas=[metadata] * len(ids)
         )
 
-    def query_vector_from_project(
+    async def query_from_collection(
             self,
-            user_email: str,
-            project_id: str,
+            collection_name: str,
             query_texts: Optional[OneOrMany[str]] = None,
             n_results: int = 10,
     ):
-        where_filter = self._generate_metadata(user_email, project_id)
-
-        return self.vector_db.query(
-            collection_name=self.collection_name,
+        return await self.vector_db.query(
+            collection_name=collection_name,
             query_texts=query_texts,
             n_results=n_results,
-            where=where_filter,
         )
 
-    def list_collections(self):
-        return self.vector_db.list_collections()
+    async def list_collections(self):
+        return await self.vector_db.list_collections()
