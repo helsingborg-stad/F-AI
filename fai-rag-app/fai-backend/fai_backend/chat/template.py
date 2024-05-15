3599442ca38db58e18d526f6e23efaedb397837e
Ehsan Zilaei <zzilaei@gmail.com>
2024-05-15 11:21:34 +0200

    fix(rag-llm): chat and streams  (#89)
    * add chat stream settings, template and prompts
    
    * fix RAG stream
    
    * add collection name parameter to endpoint `/llm-rag-question`
    
    * update QAF for new RAG method
    
    * removed unused variables and lambdas
    
    * return RAG response as a response string
    
    - Converted response from chat deltas to a string.
    
    * remove unused imports and fix stream naming
    
    * refactor stream lambdas to function calls

---
+++ fai-rag-app/fai-backend/fai_backend/chat/template.py
@@ -0,0 +1,55 @@
+import os
+from fai_backend.chat.prompt import UserChatPrompt, SystemChatPrompt
+
+CHAT_PROMPT_TEMPLATE_ARGS = {
+    "name": "ChatStream",
+    "messages": [
+        SystemChatPrompt(
+            "You are a helpful AI assistant that helps people with answering questions about planning "
+            "permission.<br> If you can't find the answer in the search result below, just say (in Swedish) "
+            "\"Tyvärr kan jag inte svara på det.\" Don't try to make up an answer.<br> If the "
+            "question is not related to the context, politely respond that you are tuned to only "
+            "answer questions that are related to the context.<br> The questions are going to be "
+            "asked in Swedish. Your response must always be in Swedish."
+        ),
+        UserChatPrompt("{query}"),
+        UserChatPrompt("Here are the results of the search:\n\n {results}"),
+    ],
+    "input_map_fn": lambda input: {
+        "query": list(input)[0]['query'],
+        "results": ' | '.join([doc for doc, _ in list(input)[0]['results']])
+    },
+    "settings": {
+        "model": os.environ.get("GPT_4_MODEL_NAME", "gpt-4"),
+        "temperature": 0
+    },
+}
+
+SCORING_PROMPT_TEMPLATE_ARGS = {
+    "name": "ScoringStream",
+    "messages": [
+        SystemChatPrompt("You are a scoring systems that classifies documents from 0-100 based on how well they answer a query."),
+        UserChatPrompt("Query: {query}\n\nDocument: {document}"),
+    ],
+    "input_map_fn": lambda input: {**(input)},
+    "settings": {
+        "model": "gpt-3.5-turbo",
+        "temperature": 0,
+        "functions": [
+            {
+                "name": "score_document",
+                "description": "Scores the previous document according to the user query\n\n    Parameters\n    ----------\n    score\n        A number from 0-100 scoring how well does the document matches the query. The higher the score, the better match for the query\n    ",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "score": {
+                            "type": "number",
+                        }
+                    },
+                    "required": ["score"],
+                }
+            }
+        ],
+        "function_call": {"name": "score_document"},
+    },
+}
--- fai-rag-app/fai-backend/fai_backend/llm/service.py
+++ fai-rag-app/fai-backend/fai_backend/llm/service.py
@@ -1,8 +1,12 @@
-import asyncio
-from typing import AsyncGenerator, Iterable, List, Tuple
+import json
 
 from langstream import Stream, join_final_output, as_async_generator
 from langstream.contrib import OpenAIChatStream, OpenAIChatMessage, OpenAIChatDelta
+
+from fai_backend.chat.stream import create_chat_stream_from_prompt
+from fai_backend.chat.template import CHAT_PROMPT_TEMPLATE_ARGS, SCORING_PROMPT_TEMPLATE_ARGS
+from fai_backend.vector.service import VectorService
+from fai_backend.vector.factory import vector_db
 
 SYSTEM_TEMPLATE = "You are a helpful AI assistant that helps people with answering questions about planning "
 "permission.<br> If you can't find the answer in the search result below, just say (in Swedish) "
@@ -12,9 +16,24 @@ SYSTEM_TEMPLATE = "You are a helpful AI assistant that helps people with answeri
 "asked in Swedish. Your response must always be in Swedish."
 
 
+async def query_vector(vector_service, collection_name, query, n_results=10):
+    vector_result = await vector_service.query_from_collection(
+        collection_name=collection_name,
+        query_texts=[query],
+        n_results=n_results,
+    )
+
+    documents = vector_result['documents'][0] if 'documents' in vector_result and vector_result['documents'] else []
+
+    return Stream[None, str](
+        "QueryVectorStream",
+        lambda _: as_async_generator(*documents)
+    )
+
+
 async def ask_llm_question(question: str):
     llm_stream: Stream[str, str] = OpenAIChatStream[str, OpenAIChatDelta](
-        "RecipeStream",
+        "AskLLMStream",
         lambda user_question: [
             OpenAIChatMessage(
                 role="system",
@@ -32,50 +51,59 @@ async def ask_llm_question(question: str):
     return await join_final_output(llm_stream(question))
 
 
-async def ask_llm_raq_question(question: str):
+async def ask_llm_raq_question(question: str, collection_name: str):
     add_document, list_documents = (lambda documents: (
         lambda document: (documents.append(document), document)[1],
         lambda: [*documents]
     ))([])
 
-    def retrieve_documents(query: str, n_results: int) -> AsyncGenerator[str, None]:
-        mock_results = [
-            "Document 1",
-            "Document 2",
-            "Document 3"
-        ][0:n_results]
+    chat_stream, _ = create_chat_stream_from_prompt(CHAT_PROMPT_TEMPLATE_ARGS)
+    scoring_stream, _ = create_chat_stream_from_prompt(SCORING_PROMPT_TEMPLATE_ARGS)
+    vector_service = VectorService(vector_db=vector_db)
 
-        return as_async_generator(*mock_results)
+    def append_score_to_documents(scores):
+        return zip(list_documents(), [s[0] for s in scores])
+
+    def sort_and_slice_documents(scored_documents, slice_size: int):
+        first_element = list(scored_documents)[0]
+        sorted_scores = sorted(first_element, key=lambda x: x[1], reverse=True)
+        return sorted_scores[:slice_size]
+
+    def create_query_document_pair(query, document):
+        return {"query": query, "document": document}
+
+    vector_db_query_result = await query_vector(
+        vector_service=vector_service,
+        collection_name=collection_name,
+        query=question,
+    )
+
+    scoring_stream = scoring_stream.map(
+        lambda delta: json.loads(delta.content)['score']
+        if delta.role == "function" and delta.name == "score_document"
+        else 0
+    )
 
     def stream(query):
-        return (
-            Stream[str, str](
-                "RetrieveDocumentsStream",
-                lambda query: retrieve_documents(query, n_results=3)
-            )
-            .map(add_document)
-            # .map(lambda document: {"query": query, "document": document})
-            # .gather()
-            # .and_then(lambda results: {"query": query, "results": results[0]})
-            .and_then(
-                OpenAIChatStream[Iterable[List[Tuple[str, int]]], OpenAIChatDelta](
-                    "AnswerStream",
-                    lambda results: [
-                        OpenAIChatMessage(
-                            role="system",
-                            content=SYSTEM_TEMPLATE,
-                        ),
-                        OpenAIChatMessage(role="user", content=query),
-                        OpenAIChatMessage(
-                            role="user",
-                            # content=f"Here are the results of the search:\n\n {' | '.join([doc for doc, _ in list(results)[0]])}",
-                            content=f"Here are the results of the search:\n\n Det behövs ingen bygglov för att sätta upp en flaggstång. Det är däremot viktigt att tänka på att flaggstången inte får vara högre än 12 meter. Om flaggstången är högre än 3 meter behöver du anmäla detta till kommunen. Det är också viktigt att tänka på att flaggstången inte får placeras så att den skymmer sikten för trafikanter eller för grannar. Om du är osäker på om du behöver anmäla flaggstången eller inte kan du kontakta kommunen för att få hjälp.",
-                        ),
-                    ],
-                    model="gpt-4",
-                    temperature=0,
-                ).map(lambda delta: delta.content)
-            )
-        )(query)
+        try:
+            return (
+                vector_db_query_result
+                .map(add_document)
+                .map(lambda document: create_query_document_pair(query, document))
+                .map(scoring_stream)
+                .gather()
+                .and_then(append_score_to_documents)
+                .and_then(lambda scored_documents: sort_and_slice_documents(scored_documents, 6))
+                .and_then(lambda results: {"query": query, "results": results[0]})
+                .and_then(chat_stream)
+                .map(lambda delta: delta.content)
+            )(query)
+        except Exception as e:
+            print(f"Error processing query: {e}", {str(e)})
+            raise e
 
-    return await join_final_output(stream(question))
+    try:
+        return await join_final_output(stream(question))
+    except Exception as e:
+        print(f"Error joining final output '{question}': {str(e)}")
+        raise e
