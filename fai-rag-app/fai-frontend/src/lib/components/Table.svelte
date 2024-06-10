5b48317f559592f969d006ee2b5fa91bf95a379c
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2024-06-10 10:03:28 +0200

    add(Table): sortable columns

--- fai-rag-app/fai-frontend/src/lib/components/Table.svelte
+++ fai-rag-app/fai-frontend/src/lib/components/Table.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import Link from "./Link.svelte";
+    import InlineSvgIcon from "./SVG.svelte"
 
     let className: string | null = null
     export {className as class}
@@ -8,11 +9,20 @@
         key: string
         label: string
         link_text?: string
+        sortable?: boolean
+        sort_url?: string | null
+        sort_order?: 'asc' | 'desc' | null
     }[] = []
     export let data: { [key: string]: any }[] = []
     export let headerClass: string = ''
     export let rowClass: string = ''
     export let cellClass: string = ''
+
+    const SORT_ICONS = {
+        'asc': 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLWNoZXZyb24tdXAiPjxwYXRoIGQ9Im0xOCAxNS02LTYtNiA2Ii8+PC9zdmc+',
+        'desc': 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLWNoZXZyb24tZG93biI+PHBhdGggZD0ibTYgOSA2IDYgNi02Ii8+PC9zdmc+',
+        'none': 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLWNoZXZyb25zLXVwLWRvd24iPjxwYXRoIGQ9Im03IDE1IDUgNSA1LTUiLz48cGF0aCBkPSJtNyA5IDUtNSA1IDUiLz48L3N2Zz4='
+    }
 </script>
 
 {#if columns.length > 0}
@@ -20,9 +30,31 @@
         <thead>
         {#key columns}
             <tr class={headerClass}>
-                {#each columns as column}
+                {#each columns as column, i}
                     <th class={cellClass}>
-                        <slot name="header" column={column}>{column.label}</slot>
+                        <div class:inline-flex={1}
+                             class:space-x-0.5={1}
+                             class:items-center={1}
+                        >
+                            {#key column}
+                                {#if column?.sortable && column?.sort_url}
+
+                                <span class:text-neutral={column?.sort_order}>
+                                <slot name="header" column={column}>{column.label}</slot>
+                            </span>
+                                    <Link href={column.sort_url} size="xs"
+                                          variant="ghost" square={true}>
+                                        <InlineSvgIcon
+                                                width="16"
+                                                state={column?.sort_order ? 'neutral' : null}
+                                                src={SORT_ICONS[column?.sort_order ?? 'none']}/>
+                                    </Link>
+
+                                {:else}
+                                    <slot name="header" column={column}>{column.label}</slot>
+                                {/if}
+                            {/key}
+                        </div>
                     </th>
                 {/each}
             </tr>
