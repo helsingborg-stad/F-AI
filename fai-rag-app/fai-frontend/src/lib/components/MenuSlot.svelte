003b0fd3c9f2663de42c6ed7649f2993dda2d469
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2024-06-17 14:50:05 +0200

    fix(Menu): support setting disable on menu item

--- fai-rag-app/fai-frontend/src/lib/components/MenuSlot.svelte
+++ fai-rag-app/fai-frontend/src/lib/components/MenuSlot.svelte
@@ -3,11 +3,14 @@
     import type {IRenderableComponent} from "../types"
 
     export let components: IRenderableComponent[] = []
+
+    let className = ''
+    export {className as class}
 </script>
 
 
 {#each components as component}
-    <li class:disabled={component?.props?.disabled === true}>
+    <li class:disabled={component?.props?.disabled === true} class={className}>
         <slot>
             <slot name="links" component={component}/>
         </slot>
