beb4fa8973cb14f5c92c4b3c34a6c191a31cf470
MrSpex <mail@dannilsson.se>
2024-08-29 01:05:17 +0200

    🔧 conf(BE): add message broker configs

--- fai-rag-app/fai-backend/fai_backend/message_broker/dependencies.py
+++ fai-rag-app/fai-backend/fai_backend/message_broker/dependencies.py
@@ -4,4 +4,5 @@ from fai_backend.message_broker.interface import IMessageQueue
 
 
 def get_message_queue() -> IMessageQueue:
-    return MessageBroker.create(settings.APP_MESSAGE_BROKER)
+    return MessageBroker.create(broker_type=settings.APP_MESSAGE_BROKER,
+                                redis_host=settings.APP_MESSAGE_BROKER_WORKER_HOST)
