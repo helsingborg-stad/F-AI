81e053376e04ba63c734e7c8f1201f8329cc4aec
MrSpex <mail@dannilsson.se>
2024-08-28 19:43:19 +0200

    🧱 (backend): remove dev code

--- fai-rag-app/fai-backend/fai_backend/message_broker/factory.py
+++ fai-rag-app/fai-backend/fai_backend/message_broker/factory.py
@@ -1,7 +1,5 @@
-import asyncio
 from typing import Type, TypeVar, Callable
 
-from fai_backend.config import settings
 from fai_backend.message_broker.interface import IMessageQueue
 from fai_backend.message_broker.memory import MemoryQueue
 from fai_backend.message_broker.redis_queue import RedisQueue
@@ -39,17 +37,3 @@ def create_queue(broker_type: Type[T], *args, **kwargs) -> IMessageQueue:
 
 MessageBroker.register('redis_queue')(lambda *args, **kwargs: create_queue(RedisQueue, *args, **kwargs))
 MessageBroker.register('memory_queue')(lambda *args, **kwargs: create_queue(MemoryQueue, *args, **kwargs))
-
-
-if __name__ == '__main__':
-    mb = MessageBroker.create(settings.APP_MESSAGE_BROKER)
-
-    for i in range(1, 11):
-        job = mb.enqueue('fai_backend.message_broker.tasks.add', i, i, i)
-
-    # async def loop():
-    #     while True:
-    #         await asyncio.sleep(1)
-    #
-    #
-    # asyncio.run(loop())
