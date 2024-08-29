415185b894782355c097e3e538bd649dcdffc647
zilaei <zzilaei@gmail.com>
2024-08-29 16:03:43 +0200

    feat(collection): create collection metadata

--- fai-rag-app/fai-backend/fai_backend/documents/routes.py
+++ fai-rag-app/fai-backend/fai_backend/documents/routes.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter, Depends, UploadFile
+from fastapi import APIRouter, Depends, UploadFile, Form
 
 from fai_backend.config import settings
+from fai_backend.collection.dependencies import get_collection_service
+from fai_backend.collection.service import CollectionService
 from fai_backend.dependencies import get_page_template_for_logged_in_users, get_project_user
 from fai_backend.files.dependecies import get_file_upload_service
 from fai_backend.files.service import FileUploadService
@@ -63,43 +65,53 @@ def list_view(
 
 
 @router.get('/documents/upload', response_model=list, response_model_exclude_none=True)
-def upload_view(
-        project_user: ProjectUser = Depends(get_project_user),
-        view=Depends(get_page_template_for_logged_in_users),
-) -> list:
+def upload_view(view=Depends(get_page_template_for_logged_in_users)) -> list:
     return view(
-        c.Form(
-            submit_as='form',
-            submit_url='/api/documents/upload_and_vectorize',
-            components=[
-                c.FileInput(
-                    name='files',
-                    title=_('file', 'File'),
-                    required=True,
-                    multiple=True,
-                    file_size_limit=settings.FILE_SIZE_LIMIT,
+        [c.Div(components=[
+            c.Div(components=[
+                c.Form(
+                    submit_as='form',
+                    submit_url='/api/documents/upload_and_vectorize',
+                    components=[
+                        c.InputField(
+                            name='collection_label',
+                            title=_('input_fileupload_collection_label',
+                                    'Collection label (optional)'),
+                            placeholder=_('input_fileupload_collection_placeholder',
+                                          'Collection label (optional)'),
+                            required=False,
+                            html_type='text',
+                        ),
+                        c.FileInput(
+                            name='files',
+                            title=_('file', 'File'),
+                            required=True,
+                            multiple=True,
+                            file_size_limit=settings.FILE_SIZE_LIMIT,
+                        ),
+                        c.Button(
+                            html_type='submit',
+                            label=_('upload', 'Upload'),
+                            class_name='btn btn-primary',
+                        ),
+                    ],
+                    class_name='card bg-base-100 w-full max-w-6xl',
                 ),
-                c.Button(
-                    html_type='submit',
-                    label=_('upload', 'Upload'),
-                    class_name='btn btn-primary',
-                ),
-            ],
-            class_name='card bg-base-100 w-full max-w-6xl',
-        ),
-        _('upload_documents', 'Upload documents'),
-    )
+            ]),
+        ])], _('upload_documents', 'Upload documents'))
 
 
 @router.post('/documents/upload_and_vectorize', response_model=list, response_model_exclude_none=True)
 async def upload_and_vectorize_handler(
         files: list[UploadFile],
+        collection_label: str = Form(None),
         project_user: ProjectUser = Depends(get_project_user),
         file_service: FileUploadService = Depends(get_file_upload_service),
         vector_service: VectorService = Depends(get_vector_service),
         view=Depends(get_page_template_for_logged_in_users),
         projects: list[ProjectResponse] = Depends(list_projects_request),
         project_service: ProjectService = Depends(get_project_service),
+        collection_service: CollectionService = Depends(get_collection_service),
 ) -> list:
     upload_path = file_service.save_files(project_user.project_id, files)
 
@@ -109,7 +121,13 @@ async def upload_and_vectorize_handler(
     parsed_files = file_service.parse_files(upload_path)
     await vector_service.add_documents_without_id_to_empty_collection(
         collection_name=upload_directory_name,
-        documents=parsed_files,
+        documents=parsed_files
+    )
+
+    await collection_service.create_collection_metadata(
+        collection_id=upload_directory_name or '',
+        label=collection_label or '',
+        description=''
     )
 
     # Fix/workaround for updating assistant file collection id until assistant editor ui is done
