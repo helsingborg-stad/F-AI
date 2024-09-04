e197d1526fe2304e9d5f5b32c4319af4229def24
MrSpex <mail@dannilsson.se>
2024-09-04 22:35:41 +0200

    ✨ feat(frontend): add file limit to upload file
    Dirty…

--- fai-rag-app/fai-frontend/src/lib/components/FileInput.svelte
+++ fai-rag-app/fai-frontend/src/lib/components/FileInput.svelte
@@ -16,12 +16,55 @@
     export let multiple: boolean | null = null;
 
     let ref: HTMLInputElement;
+    let notification: string = '';
+    const FILE_SIZE_LIMIT = 10; // MB
 
-    onMount(() => autoFocus && (ref?.focus() !== undefined))
+    onMount(() => autoFocus && (ref?.focus() !== undefined));
+
+    function calculateTotalFilesSize(files: FileList): { totalFiles: number, totalFileSize: number } {
+        const totalFiles = files.length;
+        let totalFileSize = 0;
+
+        for (let i = 0; i < totalFiles; i++) {
+            totalFileSize += files[i].size;
+        }
+
+        return { totalFiles, totalFileSize };
+    }
+
+    const handleFileChange = (event: Event) => {
+      const t = event.target as HTMLInputElement
+      if (t.files && t.files.length) {
+        const { totalFiles, totalFileSize } = calculateTotalFilesSize(t.files);
+        const maxFileSizeInBytes = FILE_SIZE_LIMIT * 1024 * 1024;
+        if (totalFileSize > maxFileSizeInBytes) {
+          notification = `Total file size is too large. Max file size is ${FILE_SIZE_LIMIT} MB`;
+          t.value = '';
+          t.files = null;
+        } else {
+          notification = '';
+        }
+        console.log(`Total Files: ${totalFiles}`);
+        console.log(`Total File Size: ${totalFileSize} bytes`);
+      }
+    };
 </script>
 
-<label
-        class:form-control={1}>
+<style>
+  .notification {
+    background-color: #ffcccc;
+    border: 1px solid #ff0000;
+    color: #ff0000;
+    border-radius: 5px;
+    margin: 1rem;
+    padding: 1rem;
+}
+</style>
+
+<label class:form-control={1}>
+    {#if notification}
+      <div class="notification">{notification}</div>
+    {/if}
     <input
             bind:this={ref}
             class:file-input={1}
@@ -38,13 +81,9 @@
             {title}
             {placeholder}
             {value}
-
-
+            on:change={handleFileChange}
     />
     {#if error}
         <div class="label label-text text-error mt-1">{error}</div>
     {/if}
 </label>
-
-
-
