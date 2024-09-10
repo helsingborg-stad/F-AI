1e917421fb82e13a84849cc1b3ce9704451a6d12
Dan Nilsson <mail@dannilsson.se>
2024-09-10 16:07:03 +0200

    Feat/expose version (#136)
    * 🧑‍💻 feat(backend): add git revision sha to header
    
    * 👷 cicd(docker): extract git head sha as file
    
    Used to display the current version

--- fai-rag-app/fai-backend/fai_backend/middleware.py
+++ fai-rag-app/fai-backend/fai_backend/middleware.py
@@ -1,5 +1,8 @@
+from pathlib import Path
 from fastapi import HTTPException, Request
 from starlette.responses import RedirectResponse
+
+GIT_REVISION_FILE = 'git_revision.txt'
 
 
 async def remove_trailing_slash(request: Request, call_next):
@@ -16,3 +19,26 @@ async def remove_trailing_slash(request: Request, call_next):
             )
     response = await call_next(request)
     return response
+
+
+async def add_git_revision_to_request_header(request: Request, call_next) -> RedirectResponse:
+    git_rev = read_git_revision_from_file(GIT_REVISION_FILE)
+    response = await call_next(request)
+
+    if git_rev:
+        response.headers['x-git-revision'] = git_rev
+
+    return response
+
+
+def read_file_contents(file_path: str, mode: str) -> str:
+    file = Path(__file__).parent / file_path
+    with file.open(mode) as f:
+        return f.read().rstrip()
+
+
+def read_git_revision_from_file(file_path: str) -> str | None:
+    try:
+        return read_file_contents(file_path, mode='r')
+    except FileNotFoundError:
+        return None
