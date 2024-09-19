b1fa20786eea94de6733004575f788abf2da0ae8
MrSpex <mail@dannilsson.se>
2024-09-19 09:09:03 +0200

    🎨 struct(backend): tidy

--- fai-rag-app/fai-backend/fai_backend/new_chat/routes.py
+++ fai-rag-app/fai-backend/fai_backend/new_chat/routes.py
@@ -20,47 +20,34 @@ router = APIRouter(
 
 
 @router.get('/chat', response_model=list, response_model_exclude_none=True)
-def chat_index_view(
-        authenticated_user: User | None = Depends(get_project_user),
-        view=Depends(get_page_template_for_logged_in_users),
-        projects: list[ProjectResponse] = Depends(list_projects_request),
-) -> list:
+def chat_index_view(authenticated_user: User | None = Depends(get_project_user),
+                    view=Depends(get_page_template_for_logged_in_users),
+                    projects: list[ProjectResponse] = Depends(list_projects_request)) -> list:
+
     if not authenticated_user:
         return [c.FireEvent(event=e.GoToEvent(url='/login'))]
 
-    assistants = [c.Assistant(
-        id=a.id,
-        name=a.meta.name,
-        project=p.id,
-        description=a.meta.description,
-        sampleQuestions=a.meta.sample_questions
-    ) for p in projects for a in p.assistants]
+    assistants = [c.Assistant(id=a.id,
+                              name=a.meta.name,
+                              project=p.id,
+                              description=a.meta.description,
+                              sampleQuestions=a.meta.sample_questions) for p in projects for a in p.assistants]
 
-    return view(
-        [c.SSEChat(
-            assistants=assistants,
-            endpoint='/api/assistant-stream'
-        )],
-        _('chat', 'Chat'),
-    )
+    return view([c.SSEChat(assistants=assistants,
+                           endpoint='/api/assistant-stream')],
+                _('chat', 'Chat'))
 
 
 @router.get('/chat/history', response_model=list, response_model_exclude_none=True)
 async def chat_history_view(view=Depends(get_page_template_for_logged_in_users),
                             history_service: ChatHistoryService = Depends(get_chat_history_service)) -> list:
     return view(
-        [c.DataTable(
-            data=history_service.get_chat_history(),
-            columns=[
-                DataColumn(
-                    key='title',
-                    id='title',
-                    display=DisplayAs.link,
-                    on_click=e.GoToEvent(url='/chat/{id}'),
-                    sortable=True,
-                    label=_('title', 'Title'),
-                )
-            ],
-        )],
+        [c.DataTable(data=history_service.get_chat_history(),
+                     columns=[DataColumn(key='title',
+                                         id='title',
+                                         display=DisplayAs.link,
+                                         on_click=e.GoToEvent(url='/chat/{id}'),
+                                         sortable=True,
+                                         label=_('title', 'Title'))])],
         _('chat_history', 'History')
     )
