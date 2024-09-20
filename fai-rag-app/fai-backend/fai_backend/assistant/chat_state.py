48fc2edca99ddd79bf44bedf68211230e3a415a9
MrSpex <mail@dannilsson.se>
2024-09-20 11:52:46 +0200

    💄 gui(frontend): add dots to the end of title

--- fai-rag-app/fai-backend/fai_backend/assistant/chat_state.py
+++ fai-rag-app/fai-backend/fai_backend/assistant/chat_state.py
@@ -25,7 +25,7 @@ class ChatStateService:
             user=chat_history_model.user,
             chat_id=str(chat_history_model.id),
             timestamp=chat_history_model.history[0].timestamp,
-            title=chat_history_model.history[0].content[0:30],  # TODO: replace with pre-generated AI title
+            title=f'{chat_history_model.history[0].content[:30]}...',  # TODO: replace with pre-generated AI title
             history=[LLMClientChatMessage(
                 timestamp=m.timestamp,
                 source=m.role,
