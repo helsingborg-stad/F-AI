d76ce5b37e3bfb8bfd00d29b2684df1875d7826c
zilaei <zzilaei@gmail.com>
2024-10-08 14:20:08 +0200

    feat(vector): refactor get_collection

--- fai-rag-app/fai-backend/fai_backend/vector/base_chromadb.py
+++ fai-rag-app/fai-backend/fai_backend/vector/base_chromadb.py
@@ -1,14 +1,28 @@
 from typing import Union, Optional, Mapping, Set
 import numpy as np
 from chromadb import ClientAPI
+import chromadb.utils.embedding_functions as embedding_functions
 
+from fai_backend.config import settings
 from fai_backend.vector.interface import IVector, OneOrMany, Embedding
 from fai_backend.vector.types import Document, Where
 
 
 class BaseChromaDB(IVector):
+    embedding_functions = embedding_functions.OpenAIEmbeddingFunction(
+        api_key=settings.OPENAI_API_KEY.get_secret_value(),
+        model_name="text-embedding-3-small"
+    )
+
     def __init__(self, client: ClientAPI):
         self.client = client
+
+
+    def _get_collection(self, collection_name: str):
+        return self.client.get_collection(
+            name=collection_name,
+            embedding_function=self.embedding_functions
+        )
 
     async def add(
             self,
@@ -19,7 +33,7 @@ class BaseChromaDB(IVector):
             documents: Optional[OneOrMany[str]] = None,
             uris: Optional[OneOrMany[str]] = None,
     ) -> None:
-        collection = self.client.get_collection(name=collection_name)
+        collection = self._get_collection(collection_name)
 
         collection.add(
             documents=documents,
@@ -36,7 +50,7 @@ class BaseChromaDB(IVector):
             metadatas: Optional[OneOrMany[Mapping[str, Union[str, int, float, bool]]]] = None,
             documents: Optional[OneOrMany[Document]] = None
     ) -> None:
-        collection = self.client.get_collection(name=collection_name)
+        collection = self._get_collection(collection_name)
         collection.update(
             documents=documents,
             embeddings=embeddings,
@@ -58,7 +72,7 @@ class BaseChromaDB(IVector):
             where: Optional[Where] = None,
 
     ) -> dict:
-        collection = self.client.get_collection(name=collection_name)
+        collection = self._get_collection(collection_name)
 
         return collection.query(
             query_embeddings=query_embeddings,
@@ -72,7 +86,7 @@ class BaseChromaDB(IVector):
             collection_name: str,
             ids: Optional[OneOrMany[str]] = None,
     ):
-        collection = self.client.get_collection(name=collection_name)
+        collection = self._get_collection(collection_name)
 
         return collection.get(
             ids=ids,
@@ -80,18 +94,13 @@ class BaseChromaDB(IVector):
 
     async def list_collections(self) -> Set[str]:
         collections = self.client.list_collections()
-
-        collection_names = set()
-        for collection in collections:
-            collection_names.add(collection.name)
-
-        return collection_names
+        return {collection.name for collection in collections}
 
     async def reset(self) -> bool:
         return self.client.reset()
 
     async def create_collection(self, collection_name: str):
-        return self.client.create_collection(name=collection_name)
+        return self.client.create_collection(name=collection_name, embedding_function=self.embedding_functions)
 
     async def delete_collection(self, collection_name: str):
         return self.client.delete_collection(name=collection_name)
