667d9731f4b56bfbdffb49e2d3f19e8ffc144a6f
MasterKenth <MasterKenth@users.noreply.github.com>
2024-10-16 13:39:18 +0200

    ✨ Feat(qaf): use assistant templates for qaf rag questions

--- fai-rag-app/fai-backend/fai_backend/chat/settings.py
+++
@@ -1,8 +0,0 @@
-from typing import TypedDict
-
-OpenAIStreamSettings = TypedDict('OpenAIStreamSettings', {
-    'model': str,
-    'temperature': float
-})
-
-default_settings = OpenAIStreamSettings(model='gpt-3.5-turbo', temperature=0)
