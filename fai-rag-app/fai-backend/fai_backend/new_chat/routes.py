7d683a9687e3941fa4c553b1aca49babcd35252b
MasterKenth <MasterKenth@users.noreply.github.com>
2024-10-16 10:55:52 +0200

    ✨ Feat(chat): hide templates starting with _ (for internal use)

--- fai-rag-app/fai-backend/fai_backend/new_chat/routes.py
+++ fai-rag-app/fai-backend/fai_backend/new_chat/routes.py
@@ -26,7 +26,6 @@ router = APIRouter(
 def chat_index_view(authenticated_user: ProjectUser | None = Depends(get_project_user),
                     view=Depends(get_page_template_for_logged_in_users),
                     projects: list[ProjectResponse] = Depends(list_projects_request)) -> list:
-
     if not authenticated_user:
         return [c.FireEvent(event=e.GoToEvent(url='/login'))]
 
@@ -34,7 +33,8 @@ def chat_index_view(authenticated_user: ProjectUser | None = Depends(get_project
                               name=a.meta.name,
                               project=p.id,
                               description=a.meta.description,
-                              sampleQuestions=a.meta.sample_questions) for p in projects for a in p.assistants]
+                              sampleQuestions=a.meta.sample_questions) for p in projects for a in p.assistants if
+                  not a.id.startswith('_')]
 
     return view([c.SSEChat(assistants=assistants)],
                 _('chat', 'Chat'))
@@ -44,19 +44,18 @@ def chat_index_view(authenticated_user: ProjectUser | None = Depends(get_project
 async def chat_history_view(view=Depends(get_page_template_for_logged_in_users),
                             chat_state_service: ChatStateService = Depends(get_chat_state_service),
                             user: ProjectUser = Depends(get_project_user)) -> list:
-
     if not user:
         return [c.FireEvent(event=e.GoToEvent(url='/login'))]
 
     states = await chat_state_service.get_states(user=user.email)
     return await chat_history_list_view(view, states)
 
+
 @router.get('/chat/{chat_id}', response_model=list, response_model_exclude_none=True)
 async def chat_view(chat_id: str,
                     view=Depends(get_page_template_for_logged_in_users),
                     chat_state_service: ChatStateService = Depends(get_chat_state_service),
                     project_user: ProjectUser = Depends(get_project_user)) -> list:
-
     chat_history = await chat_state_service.get_state(chat_id)
 
     if chat_history is None or chat_history.user != project_user.email:
@@ -70,7 +69,6 @@ async def chat_view(chat_id: str,
 async def chat_delete(chat_id: str,
                       chat_state_service: ChatStateService = Depends(get_chat_state_service),
                       project_user: ProjectUser = Depends(get_project_user)) -> list:
-
     chat_history = await chat_state_service.get_state(chat_id)
     if chat_history is None or chat_history.user != project_user.email:
         return [c.FireEvent(event=e.GoToEvent(url='/chat/history'))]
@@ -86,7 +84,6 @@ async def chat_edit(chat_id: str,
                     project_user: ProjectUser = Depends(get_project_user),
                     view: Callable[[list[Any], str | None], list[Any]] = Depends(
                         get_page_template_for_logged_in_users)) -> list:
-
     state = await chat_state_service.get_state(chat_id)
     if state is None or state.user != project_user.email:
         return [c.FireEvent(event=e.GoToEvent(url='/logout'))]
@@ -97,7 +94,6 @@ async def chat_edit(chat_id: str,
 @router.patch('/chat/edit', response_model=list, response_model_exclude_none=True)
 async def chat_edit_patch(data: ChatHistoryEditPayload,
                           chat_state_service: ChatStateService = Depends(get_chat_state_service)) -> list:
-
     old_state = await chat_state_service.get_state(data.chat_id)
     updated_state = old_state.model_copy(update={'title': data.title}, deep=True)
     await chat_state_service.update_state(data.chat_id, updated_state)
