d30bd8f45745ffb5f81ded579238564215ed6ea5
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2024-10-24 18:35:05 +0200

    fix(AppShell): close drawer on url change

--- fai-rag-app/fai-frontend/src/lib/components/AppShell.svelte
+++ fai-rag-app/fai-frontend/src/lib/components/AppShell.svelte
@@ -1,13 +1,29 @@
 <script lang="ts">
+  import { path, searchParams, url } from 'elegua'
+  export let hasDrawer: boolean | null = null
 
-    export let hasDrawer: boolean | null = null
+  let drawerIsOpen = false
+
+  const closeOnChange = (_: any) => false
+
+  $: drawerIsOpen = closeOnChange($url)
 </script>
 
-<div class:drawer={$$slots?.drawer || hasDrawer} class:lg:drawer-open={$$slots?.drawer || hasDrawer} {...$$props}>
-    {#if $$slots?.drawer || hasDrawer}
-        <input id="my-drawer-2" type="checkbox" class="drawer-toggle"/>
-        <slot name="drawer"/>
-    {/if}
+<div
+  class:drawer={$$slots?.drawer || hasDrawer}
+  class:lg:drawer-open={$$slots?.drawer || hasDrawer}
+  {...$$props}
+>
+  {#if $$slots?.drawer || hasDrawer}
+    <input
+      id="my-drawer-2"
+      type="checkbox"
+      class="drawer-toggle"
+      checked={drawerIsOpen}
+      on:change={() => (drawerIsOpen = !drawerIsOpen)}
+    />
+    <slot name="drawer" />
+  {/if}
 
-    <slot/>
+  <slot />
 </div>
