cad70446f7c89de76938ec6d53a3e224f34540a0
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2024-12-03 15:34:02 +0100

    feat(SvelteMarkdownWrapper): add custom link renderer (in order to target blank)

--- fai-rag-app/fai-frontend/src/lib/components/SvelteMarkdownWrapper.svelte
+++ fai-rag-app/fai-frontend/src/lib/components/SvelteMarkdownWrapper.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
-  import SvelteMarkdown from 'svelte-markdown';
+  import SvelteMarkdown from 'svelte-markdown'
+  import Link from '$lib/components/markdown/Link.svelte'
 
-  export let source;
-  export let options = {};
+  export let source: string
+  export let options = {}
 
   const defaultOptions = {
-    mangle: false
+    mangle: false,
   }
 
   const mergedOptions = {
     ...defaultOptions,
-    ...options
+    ...options,
   }
 </script>
 
-<SvelteMarkdown {source} options={mergedOptions}/>
+<SvelteMarkdown {source} options={mergedOptions} renderers={{ link: Link }} />
---
+++ fai-rag-app/fai-frontend/src/lib/components/markdown/Link.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let href = ''
+  export let title = undefined
+
+  const isExternalLink = (href) => {
+    try {
+      const url = new URL(href, window.location.origin)
+      return url.origin !== window.location.origin
+    } catch (e) {
+      return false
+    }
+  }
+
+  $: target = isExternalLink(href) ? '_blank' : null
+</script>
+
+<a {href} {title} {target}><slot></slot></a>
