59b8d4778260725d345935cbfa5eaf4144305f30
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2024-12-05 14:00:15 +0100

    fix(tailwind.config.js): rm require in favor of import

--- fai-rag-app/fai-frontend/tailwind.config.js
+++ fai-rag-app/fai-frontend/tailwind.config.js
@@ -1,20 +1,23 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-    content: [
-        "./index.html",
-        "./src/**/**/*.{svelte,ts,jsx,tsx,py}",
-        "./src/fai_backend/*/*.{svelte,ts,jsx,tsx,py}",
-        "../fai-backend/fai_backend/*/*.py",
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [require("@tailwindcss/typography"), require("daisyui"), require('autoprefixer')],
-    daisyui: {
-        themes: [
-            "corporate",
-            "business"
-        ],
-    },
-}
+/** @type {import("tailwindcss").Config} */
+import typography from "@tailwindcss/typography";
+import daisyui from "daisyui";
+import autoprefixer from "autoprefixer";
 
+export default {
+  content: [
+    "./index.html",
+    "./src/**/**/*.{svelte,ts,jsx,tsx,py}",
+    "./src/fai_backend/*/*.{svelte,ts,jsx,tsx,py}",
+    "../fai-backend/fai_backend/*/*.py"
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: [typography, daisyui, autoprefixer],
+  daisyui: {
+    themes: [
+      "corporate",
+      "business"
+    ]
+  }
+};
