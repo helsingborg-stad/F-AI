339c88b7be175e46086229b3c775f8a9da7cbd8e
zilaei <zzilaei@gmail.com>
2025-01-07 15:47:04 +0100

    backend: remove hello world example

--- fai-rag-app/fai-sveltekit-backend/main.py
+++ fai-rag-app/fai-sveltekit-backend/main.py
@@ -2,16 +2,6 @@ from fastapi import FastAPI, Request
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
-
 @app.post("/chat/stream/")
 async def chat_stream(request: Request):
     data = await request.json()
