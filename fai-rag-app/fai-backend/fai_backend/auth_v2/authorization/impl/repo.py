f1834a5baffefb1fd30357e7534c9a67c04cd5c1
MasterKenth <MasterKenth@users.noreply.github.com>
2025-01-24 16:12:07 +0100

    ✨ Feat(auth): check scopes (for users)

--- fai-rag-app/fai-backend/fai_backend/auth_v2/authorization/impl/repo.py
+++ fai-rag-app/fai-backend/fai_backend/auth_v2/authorization/impl/repo.py
@@ -1,8 +1,14 @@
 from fai_backend.auth_v2.authentication.models import AuthenticatedIdentity, AuthenticationType
 from fai_backend.auth_v2.authorization.models import IAuthorizationProvider
+from fai_backend.repositories import users_repo
 
 
 class RepoAuthorizationProvider(IAuthorizationProvider):
     async def has_scopes(self, identity: AuthenticatedIdentity, scopes: list[str]) -> bool:
-        # TODO
-        return True
+        user = await users_repo.get_user_by_email(identity.uid)
+
+        if user is not None:
+            permissions = user.projects[0].permissions
+            return all(key in permissions and permissions[key] is True for key in scopes)
+
+        return False
