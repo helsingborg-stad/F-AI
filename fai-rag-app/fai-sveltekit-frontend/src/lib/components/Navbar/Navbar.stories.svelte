7065f54f4a224e373a2ae5b71d10658275d3c7ef
MrSpex <mail@dannilsson.se>
2025-02-12 21:38:44 +0100

    💄 ui(navbar): add links, avatar and title

--- fai-rag-app/fai-sveltekit-frontend/src/lib/components/Navbar/Navbar.stories.svelte
+++ fai-rag-app/fai-sveltekit-frontend/src/lib/components/Navbar/Navbar.stories.svelte
@@ -6,7 +6,73 @@
 		title: 'Components/Navbar',
 		component: Navbar,
 		tags: ['autodocs'],
+		argTypes: {
+			avatarUrl: { control: 'text' },
+			avatarName: { control: 'text' },
+			menuItems: { control: 'array' },
+			currentUrlPath: { control: 'text' }
+		}
 	});
+
+	const sampleLinks = [
+		{
+			label: 'Some link 1',
+			path: '/some_link_1'
+		},
+		{
+			label: 'Some link 2',
+			path: '/some_link_2'
+		},
+		{
+			label: 'Settings',
+			path: '/settings'
+		}
+	];
 </script>
 
-<Story name="Menu button for `md` view ports" />
+<Story
+	name="Default"
+	args={{
+		menuItems: sampleLinks,
+		currentUrlPath: '/settings',
+	}}
+/>
+
+<Story
+	name="Avatar"
+	args={{
+		menuItems: sampleLinks,
+		currentUrlPath: '/settings',
+	 	avatarUrl: 'https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp',
+	 	avatarName: 'John Doe',
+	}}
+/>
+
+<Story
+	name="Title"
+	args={{
+		menuItems: sampleLinks,
+		currentUrlPath: '/some_link_1',
+	 	navbarTitle: 'My Navbar Title',
+	}}
+/>
+
+<Story
+	name="Title and Avatar"
+	args={{
+		menuItems: sampleLinks,
+		currentUrlPath: '/some_link_2',
+	 	avatarUrl: 'https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp',
+	 	avatarName: 'John Doe',
+	 	navbarTitle: 'My Navbar Title',
+	}}
+/>
+
+<Story
+	name="Avatar Name only"
+	args={{
+		menuItems: sampleLinks,
+		currentUrlPath: '/some_link_2',
+	 	avatarName: 'John Doe',
+	}}
+/>
--- fai-rag-app/fai-sveltekit-frontend/src/lib/components/Navbar/Navbar.svelte
+++ fai-rag-app/fai-sveltekit-frontend/src/lib/components/Navbar/Navbar.svelte
@@ -1,26 +1,69 @@
 <!--TODO: Remove dropdown from navbar and place it in top `+layout.page`-->
+<script lang="ts">
+	import type { MenuItem } from '$lib/types.js';
 
-<div class="navbar bg-gray-50">
-  <div class="navbar-start">
-    <div class="dropdown">
-      <div tabindex="0" role="button" class="btn btn-ghost btn-circle md:hidden">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor">
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M4 6h16M4 12h16M4 18h7" />
-        </svg>
-      </div>
-    </div>
-    <slot name="left"></slot>
-  </div>
-  <div class="navbar-end">
-    <slot name="right"></slot>
-  </div>
+	export let navbarTitle: string = '';
+
+	export let avatarUrl: URL | null = null;
+	export let avatarName: string = '';
+
+	export let menuItems: MenuItem[] = [];
+	export let currentUrlPath: string;
+
+	$: urlFirstDirectory = (() => {
+		const segments = currentUrlPath.split('/').filter(Boolean);
+		return segments.length > 0 ? `/${segments[0]}` : '/';
+	})();
+
+	const isActive = (path: string) => path === urlFirstDirectory;
+</script>
+
+<div class="navbar bg-base-200 md:px-3">
+	<div class="md:grow-0 grow">
+		<div class="dropdown">
+			<div tabindex="0" role="button" class="btn btn-ghost btn-circle md:hidden">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="h-5 w-5"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor">
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M4 6h16M4 12h16M4 18h7" />
+				</svg>
+			</div>
+			<ul class="menu dropdown-content bg-base-200 z-[1] mt-3 w-52 p-2 shadow gap-3">
+				{#each menuItems as { label, path }}
+					<li><a href="{path}" class:active={isActive(path)}>{label}</a></li>
+				{/each}
+			</ul>
+		</div>
+		<div class="grow">
+			{#if navbarTitle}
+				<span>{navbarTitle}</span>
+			{/if}
+		</div>
+	</div>
+	<div class="hidden md:flex justify-end grow">
+		<ul class="menu menu-horizontal gap-2">
+			{#each menuItems as { label, path }}
+				<li><a href="{path}" class:active={isActive(path)}>{label}</a></li>
+			{/each}
+		</ul>
+	</div>
+	<div class="flex-none">
+		{#if avatarName}
+			<div class="px-4">{avatarName}</div>
+		{/if}
+		{#if avatarUrl}
+			<div class="btn btn-ghost btn-circle avatar">
+				<div class="w-10 rounded-full">
+					<img src="{avatarUrl.toString()}" alt="{avatarName}">
+				</div>
+			</div>
+		{/if}
+	</div>
 </div>
