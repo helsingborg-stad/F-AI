8f0c8c3a08e9bfaacede1fa2771a1dd90be3f30b
zilaei <zzilaei@gmail.com>
2025-02-13 15:00:18 +0100

    feat: add menu header

---
+++ fai-rag-app/fai-sveltekit-frontend/src/lib/components/Menu/MenuHeader.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Icon from '$lib/components/Icon/Icon.svelte';
+	import { icons } from '$lib/components/Icon/icons.js';
+
+	export let title: string = '';
+	export let iconName: keyof typeof Icon;
+</script>
+
+<button class="btn">
+	{#if iconName && icons[iconName]}
+		<Icon icon={ icons[iconName] } />
+	{/if}
+	{title}
+</button>
