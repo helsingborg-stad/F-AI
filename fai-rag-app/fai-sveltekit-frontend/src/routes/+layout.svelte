2d66667fe3d2bf035cc67f3ffe0569ad410a2ed4
MrSpex <mail@dannilsson.se>
2025-02-19 09:51:42 +0100

    💄 gui(navbar): add navbar to global layout

--- fai-rag-app/fai-sveltekit-frontend/src/routes/+layout.svelte
+++ fai-rag-app/fai-sveltekit-frontend/src/routes/+layout.svelte
@@ -1,22 +1,37 @@
 <script lang="ts">
 	import '../app.css';
-	import { page } from '$app/stores'
+	import { page } from '$app/state';
 
 	import MenuSidebar from '$lib/components/Menu/MenuSidebar.svelte';
+	import Navbar from '$lib/components/Navbar/Navbar.svelte';
+
+	let { children } = $props();
+
+	const NAVBAR_TITLE = 'Folkets AI';
+
+	const navBarItems = [
+		{ label: 'Chat', path: '/chat' },
+		{ label: 'Settings', path: '/settings' }
+	];
+
+	const menuSidebarItems = [
+		{ label: 'Home', path: '/' },
+		{ label: 'Hello', path: '/hello' },
+		{ label: 'Chat', path: '/chat' },
+		{ label: 'Assistant', path: '/assistant' }
+	];
 </script>
 
-<div class="h-screen">
-	<div class="relative flex h-full overflow-hidden z-0">
-		<MenuSidebar menuItems={[
-				{label: 'Chat', path: '/chat'},
-				{label: 'Assistant', path: '/assistant'}
-			]} currentUrlPath={$page.url.pathname} />
-		<!-- Main Content -->
-		<div class="relative flex h-full w-full flex-1 flex-col overflow-hidden">
-			<!-- Page Content -->
-			<div class="flex-1 flex flex-col bg-amber-50">
-				<slot />
-			</div>
-		</div>
+<div class="h-screen flex flex-col">
+	<nav class="w-full bg-blue-100 flex md:px-4">
+		<Navbar navbarTitle={NAVBAR_TITLE} {navBarItems} currentUrlPath={page.url.pathname} />
+	</nav>
+	<div class="flex flex-grow">
+		<aside class="w-48 h-full p-4">
+			<MenuSidebar {menuSidebarItems} currentUrlPath={page.url.pathname} />
+		</aside>
+		<main class="bg-amber-100 flex-grow h-auto p-4">
+			{@render children()}
+		</main>
 	</div>
 </div>
