e84d0ed0f8e5936676442755abc73e93684f028a
zilaei <zzilaei@gmail.com>
2025-02-21 09:32:45 +0100

    feat(menu): add history item and item options

---
+++ fai-rag-app/fai-sveltekit-frontend/src/lib/components/Menu/Chat/HistoryItem.stories.svelte
@@ -0,0 +1,39 @@
+<script module lang="ts">
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import { action } from '@storybook/addon-actions'
+	import HistoryItem from '$lib/components/Menu/Chat/HistoryItem.svelte';
+
+	const { Story } = defineMeta({
+		title: 'Components/Menu/Chat',
+		component: HistoryItem,
+		tags: ['autodocs'],
+		argTypes: {
+			title: { control: 'text' },
+			itemOptions: {
+				control: 'object',
+				description: 'Array of menu options'
+			}
+		}
+	})
+</script>
+
+<Story name="Default" args={{ title: "Chat conversation with no options" }} />
+
+<Story name="With options" args={{
+	title: "Chat conversation with options",
+	itemOptions: [
+		{
+			iconName: "pencil",
+			title: "Rename",
+			onClick: (evt: Event) => {
+				action('clicked')(evt);
+			}
+		},
+		{
+			iconName: "trash",
+			title: "Delete",
+			onClick: (evt: Event) => {
+				action('clicked')(evt);
+			}
+		}
+	]}} />
---
+++ fai-rag-app/fai-sveltekit-frontend/src/lib/components/Menu/Chat/HistoryItemOptions.stories.svelte
@@ -0,0 +1,21 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import HistoryItemOptions from '$lib/components/Menu/Chat/HistoryItemOptions.svelte';
+
+
+	const { Story } = defineMeta({
+		title: 'Components/Menu/Chat/Options',
+		component: HistoryItemOptions,
+		tags: ['autodocs'],
+		argTypes: {
+			iconName: {
+				control: 'select',
+				options: ['trash']
+			},
+			title: { control: 'text' },
+			onClick: { action: 'clicked' }
+		}
+	});
+</script>
+
+<Story name="Default" args={{ iconName: "trash", title: "Delete" }} />
