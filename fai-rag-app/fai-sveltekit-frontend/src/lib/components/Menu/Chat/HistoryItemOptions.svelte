f17938ef00dad4c13023881449f9e75ff650bdb4
Ehsan Zilaei <zzilaei@gmail.com>
2025-02-24 14:36:33 +0100

    feat(menu): add chat menu components for header and history (#218)
    * feat: add menu header
    
    * fix: menu header
    
    Use DaisyUI menu component as the header.
    
    # Conflicts:
    #	fai-rag-app/fai-sveltekit-frontend/src/lib/components/Menu/Menu.svelte
    
    * feat(menu-header): uppercase title
    
    * feat(menu): add history item and item options
    
    * fix(storybook): remove select from history item story

---
+++ fai-rag-app/fai-sveltekit-frontend/src/lib/components/Menu/Chat/HistoryItemOptions.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { icons } from '$lib/components/Icon/icons.js';
+	import Icon from '$lib/components/Icon/Icon.svelte';
+
+	export let iconName: keyof typeof icons;
+	export let title: string;
+	export let onClick: () => void;
+</script>
+
+<button
+	class="flex gap-2 items-center px-4 py-1.5 text-sm cursor-pointer hover:bg-gray-100 rounded-md"
+	on:click={onClick}
+>
+	{#if iconName && icons[iconName]}
+		<Icon icon={icons[iconName]} width={16} height={16}/>
+	{/if}
+	{title}
+</button>
