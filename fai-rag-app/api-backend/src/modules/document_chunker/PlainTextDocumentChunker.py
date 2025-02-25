2980f8b365d573e09b2f5e14db5c6a9a73f5d26d
MasterKenth <MasterKenth@users.noreply.github.com>
2025-02-25 12:25:41 +0100

    🐛 Bug(api-backend): fix chunking plaintext from urls

--- fai-rag-app/api-backend/src/modules/document_chunker/PlainTextDocumentChunker.py
+++ fai-rag-app/api-backend/src/modules/document_chunker/PlainTextDocumentChunker.py
@@ -1,4 +1,5 @@
-from unstructured.partition.xlsx import partition_xlsx
+import requests
+from unstructured.partition.text import partition_text
 
 from src.common.is_url import is_url
 from src.modules.document_chunker.helpers.unstructured_element_to_chunk import unstructured_element_to_chunk
@@ -6,15 +7,18 @@ from src.modules.document_chunker.models.Chunk import Chunk
 from src.modules.document_chunker.protocols.IDocumentChunker import IDocumentChunker
 
 
-class ExcelDocumentChunker(IDocumentChunker):
+class PlainTextDocumentChunker(IDocumentChunker):
     def chunk(self, path_or_url: str) -> list[Chunk]:
         if is_url(path_or_url):
-            elements = partition_xlsx(
-                url=path_or_url,
+            response = requests.get(path_or_url)
+            elements = partition_text(
+                text=response.text,
                 ssl_verify=True,
                 unique_element_ids=True,
             )
+            for element in elements:
+                element.metadata.url = path_or_url
         else:
-            elements = partition_xlsx(filename=path_or_url, unique_element_ids=True)
+            elements = partition_text(filename=path_or_url, unique_element_ids=True)
 
         return [unstructured_element_to_chunk(element) for element in elements]
--- fai-rag-app/api-backend/src/modules/document_chunker/helpers/get_mime_type.py
+++ fai-rag-app/api-backend/src/modules/document_chunker/helpers/get_mime_type.py
@@ -9,4 +9,10 @@ def get_mime_type(file_path: str) -> str:
         response = requests.head(file_path, allow_redirects=True)
         response.raise_for_status()
         return response.headers['Content-Type'].split(';')[0]
-    return magic.from_file(file_path, mime=True)
+    mime = magic.from_file(file_path, mime=True)
+
+    # Special case since markdown is detected/indistinguishable from plain-text
+    if mime == 'text/plain' and file_path.endswith('.md'):
+        return 'text/markdown'
+
+    return mime
