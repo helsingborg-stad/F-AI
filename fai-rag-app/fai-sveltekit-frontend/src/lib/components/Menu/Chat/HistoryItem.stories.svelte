db26012a9020a1b7a4f0861d605746ea7cb608d9
zilaei <zzilaei@gmail.com>
2025-03-03 15:36:37 +0100

    fix(chat): set a more compact history tree layout

--- fai-rag-app/fai-sveltekit-frontend/src/lib/components/Menu/Chat/HistoryItem.stories.svelte
+++ fai-rag-app/fai-sveltekit-frontend/src/lib/components/Menu/Chat/HistoryItem.stories.svelte
@@ -1,39 +1,43 @@
 <script module lang="ts">
-	import { defineMeta } from '@storybook/addon-svelte-csf';
-	import { action } from '@storybook/addon-actions'
-	import HistoryItem from '$lib/components/Menu/Chat/HistoryItem.svelte';
+  import { defineMeta } from '@storybook/addon-svelte-csf'
+  import { action } from '@storybook/addon-actions'
+  import HistoryItem from '$lib/components/Menu/Chat/HistoryItem.svelte'
 
-	const { Story } = defineMeta({
-		title: 'Components/Menu/Chat/History',
-		component: HistoryItem,
-		tags: ['autodocs'],
-		argTypes: {
-			title: { control: 'text' },
-			itemOptions: {
-				control: 'object',
-				description: 'Array of menu options'
-			}
-		}
-	})
+  const { Story } = defineMeta({
+    title: 'Components/Menu/Chat/History',
+    component: HistoryItem,
+    tags: ['autodocs'],
+    argTypes: {
+      title: { control: 'text' },
+      itemOptions: {
+        control: 'object',
+        description: 'Array of menu options',
+      },
+    },
+  })
 </script>
 
-<Story name="Default" args={{ title: "Chat conversation with no options" }} />
+<Story name="Default" args={{ title: 'Chat conversation with no options' }} />
 
-<Story name="With options" args={{
-	title: "Chat conversation with options",
-	itemOptions: [
-		{
-			iconName: "pencil",
-			title: "Rename",
-			onClick: (evt: Event) => {
-				action('clicked')(evt);
-			}
-		},
-		{
-			iconName: "trash",
-			title: "Delete",
-			onClick: (evt: Event) => {
-				action('clicked')(evt);
-			}
-		}
-	]}} />
+<Story
+  name="With options"
+  args={{
+    title: 'Chat conversation with options ',
+    itemOptions: [
+      {
+        iconName: 'pencil',
+        title: 'Rename',
+        onClick: (evt: Event) => {
+          action('clicked')(evt)
+        },
+      },
+      {
+        iconName: 'trash',
+        title: 'Delete',
+        onClick: (evt: Event) => {
+          action('clicked')(evt)
+        },
+      },
+    ],
+  }}
+/>
