be3b734f334016bc8e47e48797c79a459d23ff0d
zilaei <zzilaei@gmail.com>
2025-03-04 14:51:04 +0100

    feat(menu-sidebar): update title

--- fai-rag-app/fai-sveltekit-frontend/src/lib/components/Menu/MenuSidebar.svelte
+++ fai-rag-app/fai-sveltekit-frontend/src/lib/components/Menu/MenuSidebar.svelte
@@ -2,10 +2,11 @@
   import type { IMenuItem } from '$lib/types.js'
   import Menu from '$lib/components/Menu/Menu.svelte'
 
+  export let menuSidebarTitle: string = ''
   export let menuSidebarItems: IMenuItem[] = []
   export let currentUrlPath: string = '/'
 </script>
 
 <nav>
-  <Menu items={menuSidebarItems} {currentUrlPath} />
+  <Menu title={menuSidebarTitle} items={menuSidebarItems} {currentUrlPath} />
 </nav>
--- fai-rag-app/fai-sveltekit-frontend/src/routes/+layout.svelte
+++ fai-rag-app/fai-sveltekit-frontend/src/routes/+layout.svelte
@@ -19,6 +19,17 @@
     { label: 'Chat', path: '/chat' },
     { label: 'Assistant', path: '/assistant' },
   ]
+
+  const urlFirstDirectory = $derived(() => {
+    const segments = page.url.pathname.split('/').filter(Boolean)
+    return segments.length > 0 ? `/${segments[0]}` : '/'
+  })
+
+  // TODO: Duplicate code, use single instance of code
+  const navbarTitle = $derived(() => {
+    const matchingItem = navBarItems.find((item) => item.path === urlFirstDirectory())
+    return matchingItem ? matchingItem.label : 'Folkets AI'
+  })
 </script>
 
 <div class="flex h-screen flex-col">
@@ -27,7 +38,11 @@
   </header>
   <div class="flex flex-grow bg-base-200">
     <aside class="w-60 flex-shrink-0 overflow-hidden bg-base-200 max-md:!w-0">
-      <MenuSidebar {menuSidebarItems} currentUrlPath={page.url.pathname} />
+      <MenuSidebar
+        menuSidebarTitle={navbarTitle()}
+        {menuSidebarItems}
+        currentUrlPath={page.url.pathname}
+      />
     </aside>
     <main class="m-2 flex-grow rounded-lg border bg-stone-50">
       {@render children()}
