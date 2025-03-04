b52a7a523ec7e0513462f429681bcdd7235039f8
zilaei <zzilaei@gmail.com>
2025-03-04 09:59:39 +0100

    fix: styling and remove unnecessary code
    - Menu sidebar: Set gray background.
    - Assistant layout: Remove side bar menu.

--- fai-rag-app/fai-sveltekit-frontend/src/routes/+layout.svelte
+++ fai-rag-app/fai-sveltekit-frontend/src/routes/+layout.svelte
@@ -1,61 +1,36 @@
 <script lang="ts">
-	import '../app.css';
-	import { page } from '$app/state';
+  import '../app.css'
+  import { page } from '$app/state'
 
-	import MenuSidebar from '$lib/components/Menu/MenuSidebar.svelte';
-	import Navbar from '$lib/components/Navbar/Navbar.svelte';
+  import MenuSidebar from '$lib/components/Menu/MenuSidebar.svelte'
+  import Navbar from '$lib/components/Navbar/Navbar.svelte'
 
-	let { children } = $props();
+  let { children } = $props()
 
-	const NAVBAR_TITLE = 'Folkets AI';
+  const NAVBAR_TITLE = 'Folkets AI'
 
-	const navBarItems = [
-		{ label: 'Chat', path: '/chat' },
-		{ label: 'Settings', path: '/settings' }
-	];
+  const navBarItems = [
+    { label: 'Chat', path: '/chat' },
+    { label: 'Settings', path: '/settings' },
+  ]
 
-	const menuSidebarItems = [
-		{ label: 'Home', path: '/' },
-		{ label: 'Hello', path: '/hello' },
-		{ label: 'Chat', path: '/chat' },
-		{ label: 'Assistant', path: '/assistant' }
-	];
+  const menuSidebarItems = [
+    { label: 'Home', path: '/' },
+    { label: 'Chat', path: '/chat' },
+    { label: 'Assistant', path: '/assistant' },
+  ]
 </script>
 
-<div class="root-layout">
-	<header class="root-layout-header">
-		<Navbar navbarTitle={NAVBAR_TITLE} {navBarItems} currentUrlPath={page.url.pathname} />
-	</header>
-	<div class="root-content">
-		<aside class="root-content-aside">
-			<MenuSidebar {menuSidebarItems} currentUrlPath={page.url.pathname} />
-		</aside>
-		<main class="root-content-main">
-			{@render children()}
-		</main>
-	</div>
+<div class="flex h-screen flex-col">
+  <header class="flex w-full bg-base-200 md:px-4">
+    <Navbar navbarTitle={NAVBAR_TITLE} {navBarItems} currentUrlPath={page.url.pathname} />
+  </header>
+  <div class="flex flex-grow">
+    <aside class="w-60 flex-shrink-0 overflow-hidden bg-base-200 max-md:!w-0">
+      <MenuSidebar {menuSidebarItems} currentUrlPath={page.url.pathname} />
+    </aside>
+    <main class="flex-grow bg-stone-50">
+      {@render children()}
+    </main>
+  </div>
 </div>
-
-<style lang="postcss">
-	@reference '../app.css';
-
-	.root-layout {
-		@apply h-screen flex flex-col;
-	}
-
-	.root-layout-header {
-		@apply w-full bg-base-200 flex md:px-4;
-	}
-
-	.root-content {
-		@apply flex flex-grow;
-	}
-
-	.root-content-aside {
-		@apply flex-shrink-0 overflow-hidden bg-base-100 max-md:!w-0 w-60;
-	}
-
-	.root-content-main {
-		@apply bg-stone-50 flex-grow;
-	}
-</style>
