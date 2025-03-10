815ab10c910dc522066451a575bbae48f889b27e
MasterKenth <MasterKenth@users.noreply.github.com>
2025-03-10 16:31:22 +0100

    ✨ Feat(setup): add default/setup groups

---
+++ fai-rag-app/api-backend/src/modules/setup/setup_default_groups.py
@@ -0,0 +1,21 @@
+import os
+
+from src.modules.groups.protocols.IGroupService import IGroupService
+
+
+async def setup_default_groups(group_service: IGroupService):
+    await group_service.add(
+        new_id='ff0000000000000000000000',
+        owner=os.environ['SETUP_ADMIN'],
+        label='default',
+        members=['*@*'],
+        scopes=['']
+    )
+
+    await group_service.add(
+        new_id='ff0000000000000000000001',
+        owner='',
+        label='admins',
+        members=[os.environ['SETUP_ADMIN']],
+        scopes=['']
+    )
