91341c0f046a21279989cbf9356bd0c4472ad6a2
MasterKenth <MasterKenth@users.noreply.github.com>
2025-03-11 13:27:52 +0100

    ✨ Feat(notification): add support for email (brevo)

--- fai-rag-app/api-backend/src/modules/notification/protocols/INotificationService.py
+++ fai-rag-app/api-backend/src/modules/notification/protocols/INotificationService.py
@@ -1,6 +1,8 @@
 from typing import Protocol
 
+from src.modules.notification.models.NotificationPayload import NotificationPayload
+
 
 class INotificationService(Protocol):
-    async def send(self, recipient: str, payload: str):
+    async def send(self, recipient: str, payload: NotificationPayload):
         ...
