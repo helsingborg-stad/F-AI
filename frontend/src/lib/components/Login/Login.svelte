2521118dd904b10a43cdefebd5ed350f6f3ed6d6
zilaei <zzilaei@gmail.com>
2025-04-04 16:19:01 +0200

    fix email regex

--- frontend/src/lib/components/Login/Login.svelte
+++ frontend/src/lib/components/Login/Login.svelte
@@ -19,7 +19,7 @@
   let processingForm = $state(false)
 
   let isIdValidEmail = $derived.by(() => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
 
     return emailRegex.test(id)
   })
