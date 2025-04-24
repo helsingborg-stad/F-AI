0396e7b97d1767d7afd5c0d89251c6da7599310d
zilaei <zzilaei@gmail.com>
2025-04-24 15:36:36 +0200

    add assistant page header, side menu and stories

---
+++ frontend/src/lib/components/assistant/PageHeader.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import Icon from '$lib/components/Icon/Icon.svelte'
+  import { icons } from '$lib/components/Icon/icons.js'
+
+  interface Props {
+    title: string;
+    enableCreate: boolean;
+  }
+
+  const { title = 'Assistants', enableCreate = false }: Props = $props()
+</script>
+
+<div class="py-2 px-8">
+  <div class="flex flex-row justify-between">
+    <p class="text-xl font-medium">{title}</p>
+    {#if enableCreate}
+      <button class="btn btn-sm btn-success text-white">
+        <Icon icon={icons["userPlus"]} width={20} height={20} />
+        <span class="text-s">Create</span>
+      </button>
+    {/if}
+  </div>
+</div>
--- frontend/src/routes/assistant/+page.svelte
+++ frontend/src/routes/assistant/+page.svelte
@@ -1,49 +1,20 @@
 <script lang="ts">
   import type { IAssistant } from '$lib/types.js'
+  import Divider from '$lib/components/Divider/Divider.svelte'
+  import PageHeader from '$lib/components/assistant/PageHeader.svelte'
 
   let { data } = $props<{ assistants: IAssistant[] }>()
 
   const assistants = data.assistants
   const nrOfAssistants = assistants.length
+  const enableCreate = true
 </script>
-<div class="px-8">
-  <div class="py-4 flex flex-col">
-    <div class="flex">
-      Assistants
-      <div class="divider divider-horizontal"></div>
-      {nrOfAssistants}
-    </div>
-  </div>
-  <div class="flex flex-wrap gap-4">
-    <div class="card card-compact bg-base-100 w-96 shadow-xl">
-      <figure>
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.webp"
-          alt="Empty tape" />
-      </figure>
-      <div class="card-body">
-        <h2 class="card-title">Empty tape</h2>
-        <p>Create your very own assistant!</p>
-        <div class="card-actions justify-end">
-          <a href="assistant/create" class="btn btn-primary">Create</a>
-        </div>
-      </div>
-    </div>
-    {#each assistants as assistant}
-      <div class="card card-compact bg-base-100 w-96 shadow-xl">
-        <figure>
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.webp"
-            alt={assistant.name} />
-        </figure>
-        <div class="card-body">
-          <h2 class="card-title">{assistant.name}</h2>
-          <p>{assistant.description}</p>
-          <div class="card-actions justify-end">
-            <a href="assistant/details?assistant_id={assistant.id}" class="btn btn-primary">Details</a>
-          </div>
-        </div>
-      </div>
-    {/each}
-  </div>
+
+<PageHeader title="Assistants" {enableCreate} />
+<Divider />
+<div class="flex w-full">
+  <p>List of assistants</p>
+  <Divider />
+  <p>Assistant details</p>
 </div>
+
