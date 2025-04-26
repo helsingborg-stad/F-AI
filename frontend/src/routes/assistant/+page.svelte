40d547a92985ef2260cf3f2034ed52c185f1ed2b
zilaei <zzilaei@gmail.com>
2025-04-26 11:36:38 +0200

    add assistant main page

--- frontend/src/routes/assistant/+page.svelte
+++ frontend/src/routes/assistant/+page.svelte
@@ -3,20 +3,11 @@
   import HorizontalDivider from '$lib/components/Divider/HorizontalDivider.svelte'
   import PageHeader from '$lib/components/assistant/PageHeader.svelte'
   import VerticalDivider from '$lib/components/Divider/VerticalDivider.svelte'
+  import MainPage from '$lib/components/assistant/MainPage.svelte'
 
   let { data } = $props<{ assistants: IAssistant[] }>()
 
   const assistants = data.assistants
-  const enableCreate = true
 </script>
 
-<div class="flex flex-col min-h-0 h-full">
-  <PageHeader title="Assistants" {enableCreate} />
-  <HorizontalDivider />
-  <div class="flex flex-1 gap-4">
-    <p>List of assistants</p>
-    <VerticalDivider />
-    <p>Assistant details</p>
-  </div>
-</div>
-
+<MainPage canCreateAssistant={true}/>
