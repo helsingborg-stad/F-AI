ba7b0c8443808848649c161039cfe6323176c08f
zilaei <zzilaei@gmail.com>
2025-04-26 11:36:04 +0200

    add assistant page details

---
+++ frontend/src/lib/components/assistant/AssistantDetails.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import type { IAssistant } from '$lib/types.js'
+
+  interface Props {
+    assistant?: IAssistant
+  }
+
+  let { assistant }: Props = $props()
+</script>
+
+{#if assistant}
+  <p>Assistant details</p>
+{/if}
