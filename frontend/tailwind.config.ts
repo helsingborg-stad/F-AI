5d154cf70c247d9e379b1ebdbb3ae895419fee9a
zilaei <zzilaei@gmail.com>
2025-04-26 23:05:00 +0200

    remove Tailwind forms package
    Package conflicted with DaisyUI checkbox.

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,4 +1,3 @@
-import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
 import type { Config } from 'tailwindcss';
 import daisyui from 'daisyui';
@@ -15,5 +14,5 @@ export default {
 		themes: ['light']
 	},
 
-	plugins: [typography, forms, daisyui]
+	plugins: [typography, daisyui]
 } satisfies Config;
