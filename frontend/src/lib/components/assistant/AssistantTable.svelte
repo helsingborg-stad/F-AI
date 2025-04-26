d9534df5a1838e1c6a100f4f8ac1f0ed2f8544d0
zilaei <zzilaei@gmail.com>
2025-04-26 11:36:20 +0200

    add assistant table

---
+++ frontend/src/lib/components/assistant/AssistantTable.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import type { IAssistant } from '$lib/types.js'
+
+  interface Props {
+    assistants: IAssistant[]
+    activeAssistantId?: string
+  }
+
+  let { assistants, activeAssistantId = '' }: Props = $props()
+</script>
+
+
+<div class="overflow-x-auto">
+  <table class="table">
+    <tbody>
+    {#each assistants as assistant}
+      <tr class="hover" class:bg-base-200={activeAssistantId === assistant.id}>
+        <td>
+          <div class="flex flex-col">
+            <div class="text-base">{assistant.name}</div>
+            <div class="text-xs opacity-50">{assistant.id}</div>
+          </div>
+        </td>
+      </tr>
+    {/each}
+    </tbody>
+  </table>
+</div>
