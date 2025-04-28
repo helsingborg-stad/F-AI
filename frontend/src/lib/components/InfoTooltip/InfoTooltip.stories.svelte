a78fc956197f7cc4beb18c257ffeb00f466fb4ab
zilaei <zzilaei@gmail.com>
2025-04-28 14:37:30 +0200

    add component InfoTooltip

---
+++ frontend/src/lib/components/InfoTooltip/InfoTooltip.stories.svelte
@@ -0,0 +1,21 @@
+<script module lang="ts">
+  import { defineMeta } from '@storybook/addon-svelte-csf'
+  import InfoTooltip from '$lib/components/InfoTooltip/InfoTooltip.svelte'
+
+
+  const { Story } = defineMeta({
+    title: 'Components/InfoTooltip',
+    component: InfoTooltip,
+    tags: ['autodocs'],
+  })
+</script>
+
+<Story name="Default" args={{ toolTip: 'This is a tooltip' }} />
+
+<Story name="With text as snippet">
+  <InfoTooltip toolTip="This is a tooltip">
+    <div class="text-sm">Hover on me for tooltips</div>
+  </InfoTooltip>
+</Story>
+
+
---
+++ frontend/src/lib/components/InfoTooltip/InfoTooltip.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import type { Snippet } from 'svelte'
+  import { icons } from '$lib/components/Icon/icons.js'
+  import Icon from '$lib/components/Icon/Icon.svelte'
+
+  interface Props {
+    toolTip: string
+    children?: Snippet
+  }
+
+  let { toolTip, children }: Props = $props()
+</script>
+
+<div class="tooltip" data-tip={toolTip}>
+  <div class="flex flex-row gap-2 items-center">
+    {#if children}
+      {@render children()}
+    {/if}
+    <div class="flex-shrink-0">
+      <Icon icon={icons["info"]} width={14} height={14} />
+    </div>
+  </div>
+</div>
