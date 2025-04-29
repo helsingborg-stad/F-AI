e25be27c1786a78ebbac7845a892daea92081fbb
zilaei <zzilaei@gmail.com>
2025-04-29 11:54:56 +0200

    create assistant

--- frontend/src/lib/components/assistant/PageHeader.svelte
+++ frontend/src/lib/components/assistant/PageHeader.svelte
@@ -14,10 +14,12 @@
   <div class="flex flex-row justify-between">
     <p class="text-xl font-medium">{title}</p>
     {#if canCreateAssistant}
-      <button class="btn btn-sm btn-success text-white">
+      <form method="POST" action="?/create">
+        <button type="submit" class="btn btn-sm btn-success text-white">
         <Icon icon={icons["userPlus"]} width={20} height={20} />
         <span class="text-s">Create</span>
       </button>
+      </form>
     {/if}
   </div>
 </div>
