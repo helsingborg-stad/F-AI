4fe92bcb6adbbf7a4bed628bed61f5cb086d763f
zilaei <zzilaei@gmail.com>
2025-05-01 16:55:42 +0200

    break out form in sections and add action buttons
    fix visibility
    
    add type safety to update assistant fields
    
    fix variable naming
    
    disable autocomplete for assistant name input

---
+++ frontend/src/lib/components/Form/ActionButtons.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import { icons } from '$lib/components/Icon/icons.js'
+  import Icon from '$lib/components/Icon/Icon.svelte'
+
+  interface Props {
+    canEdit: boolean;
+    canDelete: boolean;
+    canCreate: boolean;
+    onDelete: () => void;
+    onCopy: () => void;
+    onUpdate: () => void;
+  }
+
+  let { canEdit, canDelete, canCreate, onDelete, onCopy, onUpdate }: Props = $props()
+</script>
+
+{#if canEdit}
+  <div class="flex justify-between">
+    <div class="flex flex-row gap-2">
+      <div>
+        <button type="submit" class="btn btn-sm btn-error text-white" onclick={onDelete}>
+          <Icon icon={icons["trash"]} width={20} height={20} />
+        </button>
+      </div>
+      {#if canCreate}
+        <div>
+          <button type="submit" class="btn btn-sm bg-gray-400 text-white" onclick={onCopy}>
+            <Icon icon={icons["copy"]} width={20} height={20} />
+          </button>
+        </div>
+      {/if}
+    </div>
+    <div>
+      <button type="submit" class="btn btn-sm btn-success text-white" onclick={onUpdate}>
+        <Icon icon={icons["save"]} width={20} height={20} />
+        <span class="text-s">Save</span>
+      </button>
+    </div>
+  </div>
+{/if}
---
+++ frontend/src/lib/components/Form/Section.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import type { Snippet } from 'svelte'
+  import HorizontalDivider from '$lib/components/Divider/HorizontalDivider.svelte'
+
+  interface Props {
+    title: string;
+    children: Snippet
+  }
+
+  let { title, children }: Props = $props()
+</script>
+
+<div class="pt-5 space-y-2">
+  <div class="opacity-50 pb-1">{title}</div>
+  <HorizontalDivider />
+  {@render children()}
+</div>
