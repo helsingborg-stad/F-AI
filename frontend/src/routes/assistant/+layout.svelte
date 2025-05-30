1e97c0e597f02830003a55c84b56cbbde5dc39b8
zilaei <zzilaei@gmail.com>
2025-05-30 18:32:30 +0200

    feat(frontend/assistant): add layout and page

--- frontend/src/routes/assistant/+layout.svelte
+++ frontend/src/routes/assistant/+layout.svelte
@@ -1,12 +1,32 @@
-<script>
+<script lang="ts">
+  import { goto } from '$app/navigation'
   import SidebarMenu from '$lib/components/Menu/SidebarMenu.svelte'
+  import { page } from '$app/state'
 
   let { children } = $props()
+
+  let urlFirstDirectory = $derived.by(() => {
+    const segments = page.url.pathname.split('/').filter(Boolean)
+    return segments.length > 0 ? `/${segments[1]}` : '/'
+  })
+
+  const isActive = (path: string) => path === urlFirstDirectory
 </script>
 
 <div class="flex h-screen bg-base-200">
   <aside class="w-60 flex-shrink-0 overflow-hidden bg-base-200 max-md:!w-0">
-    <SidebarMenu title="Assistant" />
+    <SidebarMenu title="Assistant">
+      <div class="flex flex-col h-full gap-2">
+        <button type="button" class="btn btn-sm {isActive('/zoo') ? 'btn-neutral' : 'btn-ghost'}"
+                onclick={() => goto('/assistant/zoo')}>
+          <span class="text-s">Assistant Zoo</span>
+        </button>
+        <button type="button" class="btn btn-sm {isActive('/edit') ? 'btn-neutral' : 'btn-ghost'}"
+                onclick={() => goto('/assistant')}>
+          <span class="text-s">Your assistants</span>
+        </button>
+      </div>
+    </SidebarMenu>
   </aside>
   <main class="m-2 flex-grow rounded-lg border bg-stone-50 overflow-hidden">
     {@render children()}
---
+++ frontend/src/routes/assistant/zoo/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import AssistantZoo from '$lib/components/Assistant/View/AssistantZoo.svelte'
+  import { mockExhibits } from '$lib/components/Assistant/View/storiesMockData.js'
+</script>
+
+<div class="w-full mx-auto max-w-3xl p-4">
+  <div class="mb-6">
+    <div class="my-2 text-center text-3xl font-bold md:my-4 md:text-5xl">
+      Assistants Zoo
+    </div>
+    <div class="mx-auto w-full text-center text-sm md:text-lg md:leading-tight text-gray-500">
+      Discover all the different kind of assistants hosted by Helsingborg. Don't be afraid to click on an assistant to read more!
+    </div>
+  </div>
+  <AssistantZoo exhibits={mockExhibits} />
+</div>
