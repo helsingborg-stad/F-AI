fc4285fcec5c912c7d85c3f66370ed4bdfec0c6c
zilaei <zzilaei@gmail.com>
2025-05-31 05:42:45 +0200

    feat(frontend/assistant): fetch zoo assistants from backend

---
+++ frontend/src/routes/assistant/zoo/+page.server.ts
@@ -0,0 +1,46 @@
+import type { PageServerLoad } from '../../../../.svelte-kit/types/src/routes/$types.js'
+import { canReadAssistants } from '$lib/state/user.svelte.js'
+import type { IAssistantCard } from '$lib/types.js'
+import { fetchAllAssistants } from '$lib/utils/assistant.js'
+
+export const load: PageServerLoad = async (event) => {
+  const userCanListAssistants = canReadAssistants()
+
+  let assistantCards: IAssistantCard[] = []
+
+  if (userCanListAssistants) {
+    assistantCards = (await fetchAllAssistants(event)).map((assistant) => ({
+      avatar: `data:image/png;base64, ${assistant.avatar_base64}`,
+      title: assistant.name,
+      description: assistant.description,
+      owner: 'Helsingborg',
+      category: 'Demo',
+      starters: assistant.sample_questions,
+      conversationCount: '<100'
+    }))
+  }
+
+  const featuredExhibit =  {
+    title: 'Featured Assistants',
+    description: 'Our most popular AI assistants',
+    cards: assistantCards.filter((card) => {
+      return card.title.toLowerCase().includes('code') || card.title.toLowerCase().includes('writing')
+    }),
+  }
+
+  const mathExhibit =  {
+    title: 'Specialized Tools',
+    description: 'Domain-specific assistants',
+    cards: assistantCards.filter((card) => card.title.toLowerCase().includes('math')),
+  }
+
+  const vanillaExhibit =  {
+    title: 'Vanilla Assistants',
+    description: 'When you don\'t need anything extra',
+    cards: assistantCards.filter((card) => card.title.toLowerCase().includes('vanilla')),
+  }
+
+  return {
+    exhibits: [featuredExhibit, mathExhibit, vanillaExhibit]
+  }
+}
--- frontend/src/routes/assistant/zoo/+page.svelte
+++ frontend/src/routes/assistant/zoo/+page.svelte
@@ -1,16 +1,20 @@
 <script lang="ts">
   import AssistantZoo from '$lib/components/Assistant/View/AssistantZoo.svelte'
-  import { mockExhibits } from '$lib/components/Assistant/View/storiesMockData.js'
+
+  let { data } = $props()
+
+  const { exhibits } = data
 </script>
 
 <div class="w-full mx-auto max-w-3xl p-4">
   <div class="mb-6">
     <div class="my-2 text-center text-3xl font-bold md:my-4 md:text-5xl">
-      Assistants Zoo
+      Assistant Zoo
     </div>
     <div class="mx-auto w-full text-center text-sm md:text-lg md:leading-tight text-gray-500">
-      Discover all the different kind of assistants hosted by Helsingborg. Don't be afraid to click on an assistant to read more!
+      Discover all the different kind of assistants hosted by Helsingborg. Don't be afraid to click on an assistant to
+      read more!
     </div>
   </div>
-  <AssistantZoo exhibits={mockExhibits} />
+  <AssistantZoo {exhibits} />
 </div>
