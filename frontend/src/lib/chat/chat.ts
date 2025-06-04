c14e171a9a6b13ce0b704d0fbcbd3b7b72b013ad
MasterKenth <MasterKenth@users.noreply.github.com>
2025-06-04 10:47:17 +0200

    Feat/frontend chat bling (#259)
    * ♻️  Refactoring(frontend/chat): turn sendChatMessage into a more self-contained state machine
    
    * ✨ Feat(frontend/chat): add loader to chat while receiving response

--- frontend/src/lib/chat/chat.ts
+++ frontend/src/lib/chat/chat.ts
@@ -1,89 +1,118 @@
-export interface RealtimeChatMessage {
+import { type Readable, type Writable, writable } from 'svelte/store'
+
+export interface ChatMessage {
   source: string
   message: string
 }
 
-export interface ChatController {
-  close: () => void
+export type ChatMachineState = 'idle' | 'sending' | 'waiting' | 'responding' | 'error'
+
+export interface ChatMachine {
+  state: Readable<ChatMachineState>
+  conversationId: Readable<string | null>
+  lastMessage: Readable<ChatMessage>
+  lastError: Readable<string | null>
+  sendMessage: (message: string, assistantId: string | null, conversationId: string | null) => Promise<void>
+  stop: () => void
 }
 
-let requestClose = false
+export function useChatMachine(): ChatMachine {
+  let es: EventSource | null = null
 
-export async function sendChatMessage(
-  message: string,
-  assistantId: string | undefined,
-  conversationId: string | undefined,
-  onAddMessage: (message: RealtimeChatMessage) => void,
-  onUpdateLastMessage: (message: RealtimeChatMessage) => void,
-  onGoto: (url: string) => void,
-  onError: (error: string) => void,
-  onMessageEnd: () => void,
-): Promise<ChatController | null> {
-  requestClose = false
+  const state: Writable<ChatMachineState> = writable('idle')
+  const lastConversationId: Writable<string | null> = writable(null)
+  const lastMessage: Writable<ChatMessage> = writable({ source: '', message: '' })
+  const lastError: Writable<string | null> = writable(null)
+  let cancelAnyInProgressCalls: boolean = false
 
-  onAddMessage({ source: 'user', message: message })
-  onAddMessage({ source: 'assistant', message: '...' })
-
-  const storeMessageResponse = await fetch('/chat', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ message, conversationId }),
-  })
-
-  if (!storeMessageResponse.ok) {
-    console.error('Error storing message', storeMessageResponse)
-    onError(
-      `Error storing message (${storeMessageResponse.status}). Try again or contact support.`,
-    )
-    return null
-  }
-
-  const { messageId } = await storeMessageResponse.json()
-
-  let accumulatedResponseMessage = ''
-
-  const chatUrl = conversationId
-    ? `/chat?message=${messageId}&conversation=${conversationId}`
-    : `/chat?message=${messageId}&assistant=${assistantId}`
-
-  const es = new EventSource(chatUrl, { withCredentials: true })
-  es.onerror = (e) => {
-    console.error('chat error', e)
-    onError('Error connecting to chat. Try again or contact support.')
-    es.close()
-  }
-  es.addEventListener('chat.conversation_id', (e) => {
-    console.log('chat conversation_id', e)
-    conversationId = e.data
-    onGoto(`/chat/${e.data}`)
-  })
-  es.addEventListener('chat.message', (e) => {
-    const { source, message } = JSON.parse(e.data)
-    accumulatedResponseMessage += message
-    console.log('es message', e, accumulatedResponseMessage)
-    onUpdateLastMessage({ source, message: accumulatedResponseMessage })
-
-    if (requestClose) {
+  const close = () => {
+    if (es) {
       es.close()
-      requestClose = false
+      es = null
     }
-  })
-  es.addEventListener('chat.error', (e) => {
-    console.error('es message error', e)
-    const { message } = JSON.parse(e.data)
-    onError(message)
-    es.close()
-  })
-  es.addEventListener('chat.message_end', (e) => {
-    console.log('es message end', e)
-    es.close()
-    onMessageEnd()
-  })
+    state.set('idle')
+    cancelAnyInProgressCalls = true
+  }
 
   return {
-    close: () => {
-      console.log('es request close')
-      requestClose = true
+    state,
+    conversationId: lastConversationId,
+    lastMessage,
+    lastError,
+
+    async sendMessage(message: string, assistantId: string | null, conversationId: string | null) {
+      if (es) {
+        return
+      }
+
+      state.set('sending')
+      lastConversationId.set(conversationId)
+      lastMessage.set({ source: '', message: '' })
+      lastError.set(null)
+      cancelAnyInProgressCalls = false
+
+      const storeMessageResponse = await fetch('/chat', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ message, conversationId }),
+      })
+
+      if (!storeMessageResponse.ok) {
+        console.error('Error storing message', storeMessageResponse)
+        lastError.set(`Error storing message (${storeMessageResponse.status}). Try again or contact support.`)
+      }
+
+      const { messageId } = await storeMessageResponse.json()
+
+      const chatUrl = conversationId
+        ? `/chat?message=${messageId}&conversation=${conversationId}`
+        : `/chat?message=${messageId}&assistant=${assistantId}`
+
+      if (cancelAnyInProgressCalls) {
+        return
+      }
+
+      es = new EventSource(chatUrl, { withCredentials: true })
+
+      state.set('waiting')
+
+      let accumulatedResponseMessage = ''
+
+      es.onerror = (e) => {
+        console.error('chat error', e)
+        lastError.set('Error connecting to chat. Try again or contact support.')
+        close()
+        state.set('error')
+      }
+
+      es.addEventListener('chat.conversation_id', (e) => {
+        console.log('chat conversation_id', e)
+        lastConversationId.set(e.data)
+        state.set('responding')
+      })
+
+      es.addEventListener('chat.message', (e) => {
+        const { source, message } = JSON.parse(e.data)
+        accumulatedResponseMessage += message
+        console.log('es message', e, accumulatedResponseMessage)
+        lastMessage.set({ source, message: accumulatedResponseMessage })
+        state.set('responding')
+      })
+
+      es.addEventListener('chat.error', (e) => {
+        console.error('es message error', e)
+        const { message } = JSON.parse(e.data)
+        lastError.set(message)
+        close()
+        state.set('error')
+      })
+
+      es.addEventListener('chat.message_end', (e) => {
+        console.log('es message end', e)
+        close()
+      })
     },
+
+    stop: close,
   }
 }
