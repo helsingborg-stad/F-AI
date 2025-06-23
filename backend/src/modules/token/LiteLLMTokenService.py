0f564d8bda151eb1d658c60ce913d33759ccf342
MasterKenth <MasterKenth@users.noreply.github.com>
2025-06-23 16:36:17 +0200

    ♻️  Refactoring(backend/llm): remove obsolete code
    remove things obsolete from moving to litellm

--- backend/src/modules/token/LiteLLMTokenService.py
+++ backend/src/modules/token/LiteLLMTokenService.py
@@ -2,7 +2,6 @@ from litellm import token_counter
 
 from src.modules.assistants.protocols.IAssistantService import IAssistantService
 from src.modules.conversations.protocols.IConversationService import IConversationService
-from src.modules.token.helpers.get_conversation_assistant_model import get_conversation_assistant_model
 from src.modules.token.protocols.ITokenService import ITokenService
 
 
@@ -25,20 +24,19 @@ class LiteLLMTokenService(ITokenService):
         return tokens
 
     async def get_token_count_with_history(self, as_uid: str, conversation_id: str, message: str) -> int:
-        fetched = await get_conversation_assistant_model(
-            as_uid=as_uid,
-            assistant_service=self._assistant_service,
-            conversation_service=self._conversation_service,
-            conversation_id=conversation_id
-        )
+        conversation = await self._conversation_service.get_conversation(as_uid=as_uid, conversation_id=conversation_id)
 
-        if not fetched:
+        if not conversation:
             return -1
 
-        conversation, assistant, model = fetched
+        assistant = await self._assistant_service.get_assistant(as_uid=as_uid, assistant_id=conversation.assistant_id,
+                                                                redact_key=False)
+
+        if not assistant:
+            return -1
 
         return token_counter(
-            model,
+            model=assistant.model,
             messages=[
                          {'role': m.role, 'content': m.content} for m in conversation.messages
                      ] + [{'role': 'user', 'content': message}]
