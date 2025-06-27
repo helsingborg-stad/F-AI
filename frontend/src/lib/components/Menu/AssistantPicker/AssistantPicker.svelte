aa9f09868eaa7cea4a16ec7605be115d56756564
zilaei <zzilaei@gmail.com>
2025-06-27 13:26:54 +0200

    fix(frontend/chat): list Assistants in picker for Safari

--- frontend/src/lib/components/Menu/AssistantPicker/AssistantPicker.svelte
+++ frontend/src/lib/components/Menu/AssistantPicker/AssistantPicker.svelte
@@ -46,13 +46,12 @@
   })
 </script>
 
-
 {#if disabled}
   <button class="btn btn-sm btn-ghost m-1 pointer-events-none cursor-not-allowed">
     {selectedAssistantName}
   </button>
 {:else}
-  <div class="dropdown dropdown-top dropdown-end" bind:this={dropdownRef}>
+  <div class="dropdown dropdown-top dropdown-end dropdown-open" bind:this={dropdownRef}>
     <button
       class="btn btn-sm btn-ghost m-1"
       aria-haspopup="true"
@@ -60,7 +59,7 @@
       aria-controls="dropdown-menu"
       onclick={(e) => {
         e.stopPropagation();
-          toggleDropdown();
+        toggleDropdown();
       }}
       onkeydown={(e) => e.key === 'Enter' && toggleDropdown()}
     >
@@ -80,33 +79,30 @@
         <polyline points="6 9 12 15 18 9"></polyline>
       </svg>
     </button>
-    {#if isOpen}
-      <ul
-        id="dropdown-menu"
-        class="dropdown-content menu bg-base-100 rounded-md z-[1] w-52 p-2 shadow"
-        role="menu"
-      >
-        {#each assistants as item}
-          {#if item.menuTitle}
-            <div class="pt-1">{item.menuTitle}</div>
-          {/if}
-          {#each item.menuItems as menuItem}
-
-            <li role="menuitem">
-              <button
-                class={selectedAssistantId === menuItem.id ? 'active' : ''}
-                onclick={(e) => {
+    <ul
+      id="dropdown-menu"
+      class="dropdown-content menu bg-base-100 rounded-md z-[999] w-52 p-2 shadow"
+      class:hidden={!isOpen}
+      role="menu"
+    >
+      {#each assistants as item}
+        {#if item.menuTitle}
+          <div class="pt-1">{item.menuTitle}</div>
+        {/if}
+        {#each item.menuItems as menuItem}
+          <li role="menuitem">
+            <button
+              class={selectedAssistantId === menuItem.id ? 'active' : ''}
+              onclick={(e) => {
                 e.stopPropagation();
                 selectAssistant(menuItem);
               }}
-              >
-                {menuItem.name}
-              </button>
-            </li>
-
-          {/each}
+            >
+              {menuItem.name}
+            </button>
+          </li>
         {/each}
-      </ul>
-    {/if}
+      {/each}
+    </ul>
   </div>
 {/if}
