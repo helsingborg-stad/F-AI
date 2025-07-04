5011e24056888f54f8a7872fba1f912ee6cd8828
zilaei <zzilaei@gmail.com>
2025-07-04 13:57:13 +0200

    feat(frontend/navbar): responsive render of components

---
+++ frontend/src/lib/components/Menu/Navbar/NavbarMenu.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import type { IMenuItem } from '$lib/types.js'
+
+  interface Props {
+    navbarMenu: IMenuItem[]
+    isActive: (path: string) => boolean
+  }
+
+  let { navbarMenu, isActive }: Props = $props()
+</script>
+
+<div class="dropdown dropdown-end">
+  <div tabindex="0" role="button" class="btn btn-circle btn-ghost">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="h-5 w-5"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M4 6h16M4 12h16M4 18h7"
+      />
+    </svg>
+  </div>
+  <ul class="menu dropdown-content z-10 mt-3 w-52 gap-3 bg-base-200 p-2 shadow">
+    {#each navbarMenu as { label, path }}
+      <li><a href={path} class:active={isActive(path)}>{label}</a></li>
+    {/each}
+  </ul>
+</div>
--- frontend/src/lib/components/Navbar/Navbar.svelte
+++ frontend/src/lib/components/Navbar/Navbar.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import type { IMenuItem } from '$lib/types.js'
   import Avatar from '$lib/components/Navbar/Avatar.svelte'
+  import NavbarMenu from '$lib/components/Menu/Navbar/NavbarMenu.svelte'
 
   interface Props {
     navbarTitle: string
@@ -24,37 +25,14 @@
   const isActive = (path: string) => path === urlFirstDirectory
 </script>
 
-<nav class="navbar p-0 min-h-0">
-  <div class="grow md:grow-0">
-    <div class="dropdown">
-      <div tabindex="0" role="button" class="btn btn-circle btn-ghost md:hidden">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M4 6h16M4 12h16M4 18h7"
-          />
-        </svg>
-      </div>
-      <ul class="menu dropdown-content z-10 mt-3 w-52 gap-3 bg-base-200 p-2 shadow">
-        {#each navbarMenu as { label, path }}
-          <li><a href={path} class:active={isActive(path)}>{label}</a></li>
-        {/each}
-      </ul>
-    </div>
-    <div class="grow">
-      {#if navbarTitle}
-        <span>{navbarTitle}</span>
-      {/if}
-    </div>
+<nav class="navbar py-0 min-h-0">
+
+  <div class="ml-2 grow md:grow-0">
+    {#if navbarTitle}
+      <span>{navbarTitle}</span>
+    {/if}
   </div>
+
   <div class="hidden grow justify-end md:flex">
     <ul class="menu menu-horizontal gap-1">
       {#each navbarMenu as { label, path }}
@@ -62,5 +40,13 @@
       {/each}
     </ul>
   </div>
-  <Avatar {avatarUrl} {avatarPlaceholder} menuItems={avatarMenu} />
+
+  <div class="hidden md:flex md:flex-none">
+    <Avatar {avatarUrl} {avatarPlaceholder} menuItems={avatarMenu} />
+  </div>
+
+  <div class="md:hidden">
+    <NavbarMenu {navbarMenu} {isActive} />
+  </div>
+
 </nav>
