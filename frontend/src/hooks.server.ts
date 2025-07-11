41f4be0e4c8e9b0907fdf70a5b7d2cc07b307e95
zilaei <zzilaei@gmail.com>
2025-07-11 16:09:33 +0200

    add implement lib Paraglide

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -1,7 +1,9 @@
+import { sequence } from '@sveltejs/kit/hooks'
+import { paraglideMiddleware } from '$lib/paraglide/server.js'
 import { type Handle, redirect } from '@sveltejs/kit'
 import { setupScopes } from '$lib/server/hooks/setup-scopes.js'
 
-export const handle: Handle = async ({ event, resolve }) => {
+const handleScopes: Handle = async ({ event, resolve }) => {
   try {
     event = await setupScopes(event)
 
@@ -23,3 +25,14 @@ export const handle: Handle = async ({ event, resolve }) => {
     return redirect(303, '/login')
   }
 }
+
+const handleParaglide: Handle = ({ event, resolve }) =>
+  paraglideMiddleware(event.request, ({ request, locale }) => {
+    event.request = request
+
+    return resolve(event, {
+      transformPageChunk: ({ html }) => html.replace('%paraglide.lang%', locale),
+    })
+  })
+
+export const handle = sequence(handleScopes, handleParaglide)
