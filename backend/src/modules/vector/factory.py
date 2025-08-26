f253fba580a609a011f550f73cfbbd287d8c1ace
MasterKenth <MasterKenth@users.noreply.github.com>
2025-08-26 18:05:02 +0200

    ✨ Feat(backend/vector): add support for chromadb as a client-server connection

--- backend/src/modules/vector/factory.py
+++ backend/src/modules/vector/factory.py
@@ -1,5 +1,8 @@
+import os
+
 from src.modules.settings.protocols.ISettingsService import ISettingsService
-from src.modules.vector.ChromaDBVectorService import ChromaDBVectorService
+from src.modules.vector.ChromaDBClientVectorService import ChromaDBClientVectorService
+from src.modules.vector.ChromaDBLocalVectorService import ChromaDBLocalVectorService
 from src.modules.vector.protocols.IVectorService import IVectorService
 
 
@@ -8,4 +11,11 @@ class VectorServiceFactory:
         self._settings_service = settings_service
 
     def get(self) -> IVectorService:
-        return ChromaDBVectorService(settings_service=self._settings_service)
+        server_host = os.environ.get("CHROMADB_HOST")
+        server_port = os.environ.get("CHROMADB_PORT")
+
+        if server_host is not None and len(server_host) > 0:
+            port = int(server_port) if server_port is not None and server_port.isdigit() else int(server_port)
+            return ChromaDBClientVectorService(settings_service=self._settings_service, host=server_host, port=port)
+
+        return ChromaDBLocalVectorService(settings_service=self._settings_service)
