b357594b4cfe3c5eb25dd305ff18739648692017
MasterKenth <MasterKenth@users.noreply.github.com>
2025-10-07 14:21:07 +0200

    ✨ Feat(backend/metrics): graceful per provider error handling
    so that the entire call doesn't fail if one provider is broken

--- backend/src/modules/metrics/GenericMetricsService.py
+++ backend/src/modules/metrics/GenericMetricsService.py
@@ -1,3 +1,4 @@
+from src.modules.metrics.models.Metric import Metric
 from src.modules.metrics.protocols.IMetricsProvider import IMetricsProvider
 from src.modules.metrics.protocols.IMetricsSerializer import IMetricsSerializer
 from src.modules.metrics.protocols.IMetricsService import IMetricsService
@@ -13,5 +14,13 @@ class GenericMetricsService(IMetricsService):
         self._providers.append(provider)
 
     async def get_serialized_metrics(self) -> str:
-        all_metrics = [metric for provider in self._providers for metric in await provider.get_metrics()]
+        all_metrics = [metric for provider in self._providers for metric in await self._try_get_metrics(provider)]
         return self._serializer.serialize(all_metrics)
+
+    @staticmethod
+    async def _try_get_metrics(provider: IMetricsProvider) -> list[Metric]:
+        try:
+            return await provider.get_metrics()
+        except Exception as e:
+            print(f"Error getting metrics from {provider}: {e}")
+            return []
